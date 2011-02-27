baea88e6d658ffeb3f109f5eeed769a4c5b8dc0d
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2011-02-27 15:56:44 +0000

    Added stream gobblers.

--- trunk/ulrice-webstarter/src/main/java/net/ulrice/webstarter/tasks/StartApplication.java
+++ trunk/ulrice-webstarter/src/main/java/net/ulrice/webstarter/tasks/StartApplication.java
@@ -1,7 +1,12 @@
 package net.ulrice.webstarter.tasks;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,70 +17,105 @@ public class StartApplication extends AbstractTask {
 
 	@Override
 	public boolean doTask(ProcessThread thread) {
-		
+
 		LinkedList<String> classPath = thread.getContext().getValue(ProcessContext.CLASSPATH, new LinkedList<String>());
-		
+
 		StringBuffer commandBuffer = new StringBuffer();
 		String localDir = thread.getAppDescription().getLocalDir();
 		String localJre = getParameterAsString("localJre");
-		if(localJre != null) {
+		if (localJre != null) {
 			commandBuffer.append(localDir).append(localJre).append(File.separator).append("bin").append(File.separator);
 		}
 		commandBuffer.append("java ");
-		
+
 		commandBuffer.append("-cp ");
-		for(String element : classPath) {
+		for (String element : classPath) {
 			commandBuffer.append(element).append(";");
 		}
 		commandBuffer.append(" ");
 
-
 		String mainClass = getParameterAsString("mainClass");
-		if(mainClass != null) {
+		if (mainClass != null) {
 			commandBuffer.append(mainClass);
 		}
-		
+
 		List<String> appParameters = thread.getAppDescription().getAppParameters();
-		if(appParameters != null) {
-			for(String appParameter : appParameters) {
+		if (appParameters != null) {
+			for (String appParameter : appParameters) {
 
 				String param = replacePlaceholders(thread, appParameter);
-				if(param != null)  {
+				if (param != null) {
 					commandBuffer.append(" ").append(param);
 				}
 			}
 		}
-		
-		try {
+
+		try {			
 			Process process = Runtime.getRuntime().exec(commandBuffer.toString(), new String[0], new File(localDir));
+			StreamGobbler isGobbler = new StreamGobbler(process.getInputStream(), System.out);
+			StreamGobbler esGobbler = new StreamGobbler(process.getErrorStream(), System.err);
+			isGobbler.start();
+			esGobbler.start();
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		System.out.println(commandBuffer);
-		
+
 		return true;
 	}
 
 	protected String replacePlaceholders(ProcessThread thread, String appParameter) {
 		appParameter = appParameter.replace("${USERID}", thread.getContext().getValueAsString(ProcessContext.USERID, ""));
-//		appParameter = appParameter.replace("${PD-H-SESSION-ID}", thread.getContext().getValueAsString(ProcessContext.COOKIE));
-//		appParameter = appParameter.replace("${PD-H-SESSION-ID}", thread.getContext().getValueAsString(ProcessContext.COOKIE));
-//		
+		// appParameter = appParameter.replace("${PD-H-SESSION-ID}",
+		// thread.getContext().getValueAsString(ProcessContext.COOKIE));
+		// appParameter = appParameter.replace("${PD-H-SESSION-ID}",
+		// thread.getContext().getValueAsString(ProcessContext.COOKIE));
+		//		
 		int cookieIdxStart = appParameter.indexOf("${COOKIE=");
 		int cookieIdxEnd = appParameter.indexOf("}", cookieIdxStart);
-		if(cookieIdxStart >= 0 && cookieIdxEnd >= 0) {
+		if (cookieIdxStart >= 0 && cookieIdxEnd >= 0) {
 			String key = appParameter.substring(cookieIdxStart, cookieIdxEnd + 1);
 			String cookiePlaceholder = appParameter.substring(cookieIdxStart + "${COOKIE=".length(), cookieIdxEnd);
 			String replacement = thread.getContext().getCookieMap().get(cookiePlaceholder);
-			if(replacement == null) {
+			if (replacement == null) {
 				return null;
 			}
 			appParameter = appParameter.replace(key, replacement);
-		}		
+		}
 		return appParameter;
 	}
 
+	class StreamGobbler extends Thread {
+
+		InputStream is;
+		OutputStream os;
+
+		public StreamGobbler(InputStream is, OutputStream os) {
+			this.is = is;
+			this.os = os;
+		}
+
+		@Override
+		public void run() {
+
+			PrintWriter pw = new PrintWriter(os);
+			InputStreamReader reader = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(reader);
+
+			String line = null;
+			try {
+				while ((line = br.readLine()) != null) {
+					pw.println(line);
+				}
+			} catch (IOException e) {
+				// TODO
+				e.printStackTrace();
+			}
+
+		}
+
+	}
 }
