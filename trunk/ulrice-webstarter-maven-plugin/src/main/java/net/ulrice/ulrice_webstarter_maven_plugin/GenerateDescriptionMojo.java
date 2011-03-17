dfb4d894219d6e4a9af065311910949e0f25aadc
apunahassaphemapetilon@hotmail.com <apunahassaphemapetilon@hotmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2011-03-17 10:33:46 +0000

    replaced directory -> directories (List of Files) added targetDirectory -> optional Parameter, if set all file matching the file name pattern within the directories will be copied into this directory

--- trunk/ulrice-webstarter-maven-plugin/src/main/java/net/ulrice/ulrice_webstarter_maven_plugin/GenerateDescriptionMojo.java
+++ trunk/ulrice-webstarter-maven-plugin/src/main/java/net/ulrice/ulrice_webstarter_maven_plugin/GenerateDescriptionMojo.java
@@ -1,14 +1,21 @@
 package net.ulrice.ulrice_webstarter_maven_plugin;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.nio.channels.FileChannel;
+import java.util.List;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
 /**
+ * Goal which touches a timestamp file.
+ * 
  * @goal create-description
  * 
  * @phase package
@@ -16,12 +23,12 @@ import org.apache.maven.plugin.MojoExecutionException;
 public class GenerateDescriptionMojo extends AbstractMojo {
 
 	/**
-	 * Location of the file.
+	 * Location of the files.
 	 * 
 	 * @parameter expression="${project.build.directory}"
 	 * @required
 	 */
-	private File directory;
+	private List<File> directories;
 
 	/**
 	 * Reg-Ex used for filter the files that should be added to the description
@@ -47,52 +54,121 @@ public class GenerateDescriptionMojo extends AbstractMojo {
 	 */
 	private URL baseUrl;
 
+	/**
+	 * Location where the files matching the filename
+	 * pattern within the directories should be copied to
+	 * 
+	 * @parameter
+	 */
+	private File targetDirectory;
+
 	public void execute() throws MojoExecutionException {
+		
+		File targetDir = targetDirectory;
+		
+		boolean copyFiles = true;
+		
+		if (targetDir != null && !targetDir.exists()) {
+			targetDir.mkdirs();
+		}
+		else {
+			copyFiles = false;
+		}
 
-		File f = directory;
+		List<File> dirs = directories;
 
-		if (!f.exists()) {
-			f.mkdirs();
+		for (File f : dirs) {
+			if (!f.exists()) {
+				f.mkdirs();
+			}
 		}
 
 		File descrFile = outputFilename;
 		PrintWriter pw = null;
-		
+
 		try {
 			pw = new PrintWriter(descrFile);
 			getLog().info("Writing to " + descrFile);
 
-			File[] files = f.listFiles();
-			if (files != null) {
-				pw.println("<tasklist>");
-				for (File file : files) {
-					String filename = file.getName();
-					
-					boolean matches = false;
-					if(filenameFilters != null) {
-						for(String filter : filenameFilters) {
-							matches |= filename.matches(filter);
-							if(matches) {
-								break;
+			boolean fileFound = false;
+
+			for (File f : dirs) {
+				getLog().info("Scanning : " + f.getAbsolutePath());
+
+				File[] files = f.listFiles();
+				if (files != null) {
+					for (File file : files) {
+						String filename = file.getName();
+
+						boolean matches = false;
+						if (filenameFilters != null) {
+							for (String filter : filenameFilters) {
+								matches |= filename.matches(filter);
+								if (matches) {
+									break;
+								}
+							}
+						} else {
+							matches = true;
+						}
+
+						if (matches) {
+							if (!fileFound) {
+								pw.println("<tasklist>");
+								fileFound = true;
+							}
+							URL url = new URL(baseUrl, filename);
+							getLog().info(
+									"-Adding file '" + file + "' as '" + url
+											+ "'.");
+							pw
+									.println("<task type=\"DownloadFile\" classpath=\"true\" url=\""
+											+ url + "\"/>");
+							if (copyFiles) {
+								try {
+									copyFile(file, targetDir);
+								}
+								catch (IOException e) {
+									throw new MojoExecutionException("Error closing FileChannels copying file " + file.getAbsolutePath());
+								}
 							}
 						}
-					} else {
-						matches = true;
-					}
-					
-					if(matches) {
-						URL url = new URL(baseUrl, filename);					
-						getLog().info("-Adding file '" + file + "' as '" + url + "'.");					
-						pw.println("<task type=\"DownloadFile\" classpath=\"true\" url=\"" + url + "\"/>");
 					}
 				}
+			}
+			if (fileFound) {
 				pw.println("</tasklist>");
 			}
 		} catch (IOException e) {
-			throw new MojoExecutionException("Error creating file " + descrFile, e);
+			throw new MojoExecutionException(
+					"Error creating file " + descrFile, e);
 		} finally {
 			if (pw != null) {
 				pw.close();
+			}
+		}
+	}
+
+	private void copyFile(File file, File targetDir) throws IOException, MojoExecutionException {
+		FileChannel inChannel = null;
+		FileChannel outChannel = null;
+		try {
+			inChannel = new FileInputStream(file).getChannel();
+			outChannel = new FileOutputStream(new File(targetDir.getAbsolutePath() + file.getName())).getChannel();
+			inChannel.transferTo(0, inChannel.size(), outChannel);
+		} catch (FileNotFoundException e) {
+			throw new MojoExecutionException(
+					"Error creating file " + targetDir, e);
+		} catch (IOException e) {
+			throw new MojoExecutionException(
+					"Error writing to file " + targetDir, e);
+		}
+		finally {
+			if (inChannel != null) {
+				inChannel.close();
+			}
+			if (outChannel != null) {
+				outChannel.close();
 			}
 		}
 	}
