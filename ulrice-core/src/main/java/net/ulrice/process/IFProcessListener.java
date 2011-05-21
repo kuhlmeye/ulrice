fa93e841b2d32f73462918b4711a3f733bf12efd
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com>
2011-05-21 19:52:06 +0000

    Extended documentation of process manager.

--- ulrice-core/src/main/java/net/ulrice/process/IFProcessListener.java
+++ ulrice-core/src/main/java/net/ulrice/process/IFProcessListener.java
@@ -4,8 +4,17 @@ import java.util.EventListener;
 
 public interface IFProcessListener extends EventListener {
 
+	/**
+	 * Called, if the state of the process changed.
+	 * 
+	 * @param process The process.
+	 */
 	public void stateChanged(IFBackgroundProcess process);
 	
+	/**
+	 * Called, if the progress of the process changed.
+	 * 
+	 * @param process The process.
+	 */
 	public void progressChanged(IFBackgroundProcess process);
-
 }
