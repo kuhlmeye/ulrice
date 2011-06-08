98289d8ed21976cbcb24142eeb854d0603344643
apunahassaphemapetilon@hotmail.com <apunahassaphemapetilon@hotmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2011-06-08 07:33:34 +0000

    [maven-release-plugin]  copy for tag ulrice-0.2

---
+++ tags/ulrice-0.2/ulrice-databinding/src/main/java/net/ulrice/databinding/impl/am/FilterMode.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package net.ulrice.databinding.impl.am;
+
+/**
+ * @author christof
+ *
+ */
+public enum FilterMode {
+    
+    NoFilter,
+    
+    
+    RegEx,
+    
+    Numeric;
+
+}
