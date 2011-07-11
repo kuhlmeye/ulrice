5bf3365e1b752305ced32aac49338599b367a2cb
dritonshoshi@gmail.com <dritonshoshi@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2011-07-11 13:32:30 +0000

    renamed NullConverter to DoNothingConverter

--- trunk/ulrice-sample/src/main/java/net/ulrice/sample/module/databinding/VDataBinding.java
+++ trunk/ulrice-sample/src/main/java/net/ulrice/sample/module/databinding/VDataBinding.java
@@ -9,7 +9,7 @@ import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
-import net.ulrice.databinding.impl.converter.NullConverter;
+import net.ulrice.databinding.converter.impl.DoNothingConverter;
 import net.ulrice.databinding.impl.ga.TableGA;
 import net.ulrice.databinding.impl.ga.TextFieldGA;
 import net.ulrice.module.IFView;
@@ -20,7 +20,9 @@ import net.ulrice.module.IFView;
  */
 public class VDataBinding extends JPanel implements IFView {
 
-	private TextFieldGA<String> textFieldGA1;
+	/** generated serialVersionUID */
+    private static final long serialVersionUID = -8885808334849700407L;
+    private TextFieldGA<String> textFieldGA1;
 	private TextFieldGA<String> textFieldGA2;
     private TableGA listGA;
 
@@ -29,8 +31,8 @@ public class VDataBinding extends JPanel implements IFView {
 	 */
 	@Override
 	public void initialize() {
-		textFieldGA1 = new TextFieldGA<String>("name", new NullConverter<String>());
-		textFieldGA2 = new TextFieldGA<String>("name", new NullConverter<String>());
+		textFieldGA1 = new TextFieldGA<String>("name", new DoNothingConverter());
+		textFieldGA2 = new TextFieldGA<String>("name", new DoNothingConverter());
 		
 		listGA = new TableGA("list");
 		
