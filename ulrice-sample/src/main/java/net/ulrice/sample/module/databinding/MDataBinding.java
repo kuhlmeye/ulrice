ce83b4b8fae98f1dac4d57a27924a359ef26b2f5
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com>
2011-07-11 10:33:56 +0000

    Consolidated simple model access

--- ulrice-sample/src/main/java/net/ulrice/sample/module/databinding/MDataBinding.java
+++ ulrice-sample/src/main/java/net/ulrice/sample/module/databinding/MDataBinding.java
@@ -9,8 +9,8 @@ import java.util.List;
 import net.ulrice.databinding.impl.am.ColumnDefinition;
 import net.ulrice.databinding.impl.am.GenericAM;
 import net.ulrice.databinding.impl.am.ListAM;
-import net.ulrice.databinding.impl.da.ReflectionDA;
-import net.ulrice.databinding.impl.validation.RegExValidator;
+import net.ulrice.databinding.modelaccess.impl.ReflectionDA;
+import net.ulrice.databinding.validation.impl.RegExValidator;
 import net.ulrice.module.IFModel;
 
 /**
@@ -30,7 +30,7 @@ public class MDataBinding implements IFModel {
 	 */
 	@Override
 	public void initialize() {
-		nameAM = new GenericAM<String>("name", new ReflectionDA<String>(this, "name"));
+		nameAM = new GenericAM<String>("name", new ReflectionDA(this, "name"));
 		nameAM.setValidator(new RegExValidator<String>("(hallo|hi)", "Validation failed. Only 'hallo' or 'hi' is allowed"));
 		name = "hallo";
 
@@ -38,10 +38,10 @@ public class MDataBinding implements IFModel {
         personList.add(new Person("Petra", "Musterfrau", 20));
         personList.add(new Person("Otto", "Normal", 20));
         
-        listAM = new ListAM<List<Person>, Person>("list", new ReflectionDA<List<Person>>(this, "personList"));
-        listAM.addColumn(new ColumnDefinition<String>("lastName", new ReflectionDA<String>("lastName"), String.class));
-        listAM.addColumn(new ColumnDefinition<String>("firstName", new ReflectionDA<String>("firstName"), String.class));
-        listAM.addColumn(new ColumnDefinition<Integer>("age", new ReflectionDA<Integer>("age"), Integer.class));
+        listAM = new ListAM<List<Person>, Person>("list", new ReflectionDA(this, "personList"));
+        listAM.addColumn(new ColumnDefinition<String>("lastName", new ReflectionDA("lastName"), String.class));
+        listAM.addColumn(new ColumnDefinition<String>("firstName", new ReflectionDA("firstName"), String.class));
+        listAM.addColumn(new ColumnDefinition<Integer>("age", new ReflectionDA("age"), Integer.class));
 	}
 
 	/**
