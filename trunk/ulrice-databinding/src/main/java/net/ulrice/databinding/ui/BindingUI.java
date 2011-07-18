f595d2708420e80350f02580d4c9a76966c04cbc
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2011-07-18 07:08:27 +0000

    Hard coded background colors are now under control of bindungui

--- trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/ui/BindingUI.java
+++ trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/ui/BindingUI.java
@@ -6,6 +6,14 @@ import javax.swing.UIManager;
 
 public class BindingUI {
 
+	public static final String BACKGROUND_STATE_MARKER_INVALID = "Ulrice.Binding.BackgroundStateMarker.InvalidBackground";
+	public static final String BACKGROUND_STATE_MARKER_CHANGED = "Ulrice.Binding.BackgroundStateMarker.ChangedBackground";
+
+	public static final String BACKGROUND_NORMAL_ODD_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.NormalOddBackground";
+	public static final String BACKGROUND_NORMAL_EVEN_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.NormalOddBackground";
+	public static final String BACKGROUND_READONLY_ODD_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.ReadonlyOddBackground";
+	public static final String BACKGROUND_READONLY_EVEN_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.ReadonlyOddBackground";
+	
 	public static Color getColor(String key, Color defaultColor) {
 		Color color = UIManager.getColor(key);
 		return color == null ? defaultColor : color;
