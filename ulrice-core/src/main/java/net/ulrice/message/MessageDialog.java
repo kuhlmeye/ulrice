4c5dc0cec6e8e3d358f706f501cea6bad6fc0530
dritonshoshi@gmail.com <dritonshoshi@gmail.com>
2011-08-02 10:28:17 +0000

    replaced JLabel by JTextArea

--- ulrice-core/src/main/java/net/ulrice/message/MessageDialog.java
+++ ulrice-core/src/main/java/net/ulrice/message/MessageDialog.java
@@ -1,13 +1,10 @@
-/**
- * created by: DL10KUH created at: 22.10.2010, 14:40:04
- */
 package net.ulrice.message;
 
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
 import javax.swing.JDialog;
-import javax.swing.JLabel;
+import javax.swing.JTextArea;
 import javax.swing.UIManager;
 
 import net.ulrice.ui.UI;
@@ -22,26 +19,27 @@ public class MessageDialog extends JDialog {
     /** Generated serialVersionUID */
     private static final long serialVersionUID = -707670329224887436L;
 
-    JLabel label = new JLabel();
+    JTextArea textArea = new JTextArea();
     
     public MessageDialog() {
         setUndecorated(true);
-        add(label);
-        label.setOpaque(UIManager.getBoolean(UI.MESSAGEDIALOG_OPAQUE));
-        label.setForeground(UIManager.getColor(UI.MESSAGEDIALOG_FOREGROUND));
+        add(textArea);
+        textArea.setEditable(false);
+        textArea.setOpaque(UIManager.getBoolean(UI.MESSAGEDIALOG_OPAQUE));
+        textArea.setForeground(UIManager.getColor(UI.MESSAGEDIALOG_FOREGROUND));
         
 //        URL resource = getClass().getResource("warning32.png");
 //        ImageIcon imageIcon = new ImageIcon(resource);
 
 //        label.setIcon(imageIcon);
-        label.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.RED, 1), BorderFactory
+        textArea.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createLineBorder(Color.RED, 1), BorderFactory
             .createEmptyBorder(5, 5, 5, 5)));
 
-        label.setOpaque(true);
+        textArea.setOpaque(true);
     }
 
     public void setMessage(String message) {
-        label.setText(message);
+        textArea.setText(message);
         
     }
 }
