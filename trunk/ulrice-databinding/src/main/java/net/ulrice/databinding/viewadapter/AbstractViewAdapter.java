6befbf4a86dda8117437455f55f3b1dc5844644b
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2011-08-26 09:41:17 +0000

    Readonly textfields can now use presentations paths to display string from a complex object.

--- trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/AbstractViewAdapter.java
+++ trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/AbstractViewAdapter.java
@@ -7,137 +7,132 @@ import net.ulrice.databinding.IFBinding;
 import net.ulrice.databinding.converter.IFValueConverter;
 import net.ulrice.databinding.converter.impl.DoNothingConverter;
 
+public abstract class AbstractViewAdapter<M, V> implements IFViewAdapter<M, V> {
 
-
-
-public abstract class AbstractViewAdapter <M, V> implements IFViewAdapter <M, V> {
-
-    private final List<IFViewChangeListener> _listeners = new ArrayList<IFViewChangeListener> ();
+    private final List<IFViewChangeListener> _listeners = new ArrayList<IFViewChangeListener>();
 
     private boolean inNotification = false;
 
-	private Class<V> viewType;
-	
-	private IFTooltipHandler tooltipHandler;
-	private IFStateMarker stateMarker;
-	private IFValueConverter<M, V> valueConverter;
-	private boolean bindWithoutValue;
-	private boolean useAutoValueConverter = true;
+    private Class<V> viewType;
+
+    private IFTooltipHandler tooltipHandler;
+    private IFStateMarker stateMarker;
+    private IFValueConverter<M, V> valueConverter;
+    private boolean bindWithoutValue;
+    private boolean useAutoValueConverter = true;
 
     @SuppressWarnings({ "rawtypes", "unchecked" })
     public AbstractViewAdapter(Class viewType) {
-		this.viewType = viewType;
-		setValueConverter(null);
-	}
+        this.viewType = viewType;
+        setValueConverter(null);
+    }
 
-	protected void fireViewChange () {
-    	inNotification = true;
-        for (IFViewChangeListener l: _listeners) {
-            l.viewValueChanged (this);
+    protected void fireViewChange() {
+        inNotification = true;
+        if (isEnabled()) {
+            for (IFViewChangeListener l : _listeners) {
+                l.viewValueChanged(this);
+            }
         }
-    	inNotification = false;
+        inNotification = false;
     }
-	
-	@Override
-	public void updateFromBinding(IFBinding binding) {
-		if(!isInNotification()) {
-			removeComponentListener();
-			setValue((M) binding.getCurrentValue());
-			addComponentListener();
-		}
-		if(getTooltipHandler() != null) {
-			getTooltipHandler().updateTooltip(binding, getComponent());
-		}
-		if(getStateMarker() != null) {
-			getStateMarker().updateState(binding, getComponent());
-		}
-	}
-    
-    protected abstract void addComponentListener();
-
-	protected abstract void setValue(M value);
-
-	protected abstract void removeComponentListener();
-
-	@Override
-    public void addViewChangeListener (IFViewChangeListener l) {
-        _listeners.add (l);
-    }
-
 
     @Override
-    public void removeViewChangeListener (IFViewChangeListener l) {
-        _listeners.remove (l);
+    public void updateFromBinding(IFBinding binding) {
+        if (!isInNotification()) {
+            removeComponentListener();
+            setValue((M) binding.getCurrentValue());
+            addComponentListener();
+        }
+        if (getTooltipHandler() != null) {
+            getTooltipHandler().updateTooltip(binding, getComponent());
+        }
+        if (getStateMarker() != null) {
+            getStateMarker().updateState(binding, getComponent());
+        }
     }
-    
+
+    protected abstract void addComponentListener();
+
+    protected abstract void setValue(M value);
+
+    protected abstract void removeComponentListener();
+
+    @Override
+    public void addViewChangeListener(IFViewChangeListener l) {
+        _listeners.add(l);
+    }
+
+    @Override
+    public void removeViewChangeListener(IFViewChangeListener l) {
+        _listeners.remove(l);
+    }
+
     protected boolean isInNotification() {
-    	return inNotification;
+        return inNotification;
     }
-    
 
-	@Override
-	public Class<V> getViewType() {
-		return viewType;
-	}
+    @Override
+    public Class<V> getViewType() {
+        return viewType;
+    }
 
-	@Override
-	public void setTooltipHandler(IFTooltipHandler tooltipHandler) {
-		this.tooltipHandler = tooltipHandler;		
-	}
+    @Override
+    public void setTooltipHandler(IFTooltipHandler tooltipHandler) {
+        this.tooltipHandler = tooltipHandler;
+    }
 
-	@Override
-	public void setStateMarker(IFStateMarker stateMarker) {
-		this.stateMarker = stateMarker;
-		if(this.stateMarker != null) {
-			this.stateMarker.initialize(getComponent());
-		}
-	}
+    @Override
+    public void setStateMarker(IFStateMarker stateMarker) {
+        this.stateMarker = stateMarker;
+        if (this.stateMarker != null) {
+            this.stateMarker.initialize(getComponent());
+        }
+    }
 
-	protected IFTooltipHandler getTooltipHandler() {
-		return tooltipHandler;
-	}
+    protected IFTooltipHandler getTooltipHandler() {
+        return tooltipHandler;
+    }
 
-	protected IFStateMarker getStateMarker() {
-		return stateMarker;
-	}
+    protected IFStateMarker getStateMarker() {
+        return stateMarker;
+    }
 
-	public boolean isBindWithoutValue() {
-		return bindWithoutValue;
-	}
+    public boolean isBindWithoutValue() {
+        return bindWithoutValue;
+    }
 
-	public void setBindWithoutValue(boolean bindWithoutValue) {
-		this.bindWithoutValue = bindWithoutValue;
-	}
+    public void setBindWithoutValue(boolean bindWithoutValue) {
+        this.bindWithoutValue = bindWithoutValue;
+    }
 
-	@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     public void setValueConverter(IFValueConverter<M, V> valueConverter) {
-		this.valueConverter = valueConverter;
-		if (this.valueConverter == null) {
-		    this.valueConverter = DoNothingConverter.INSTANCE;
-		}
-	}
-	
+        this.valueConverter = valueConverter;
+        if (this.valueConverter == null) {
+            this.valueConverter = DoNothingConverter.INSTANCE;
+        }
+    }
 
-	@Override
-	public IFValueConverter<M, V> getValueConverter() {
-		return valueConverter;
-	}
+    @Override
+    public IFValueConverter<M, V> getValueConverter() {
+        return valueConverter;
+    }
 
-    
-	protected M viewToModel(V object) {
-	    return getValueConverter().viewToModel(object);
-	}
+    protected M viewToModel(V object) {
+        return getValueConverter().viewToModel(object);
+    }
 
     protected V modelToView(M object) {
         return getValueConverter().modelToView(object);
-	}
-    
+    }
+
     @Override
     public boolean isUseAutoValueConverter() {
-    	return useAutoValueConverter;
+        return useAutoValueConverter;
     }
-        
+
     public void setUseAutoValueConverter(boolean useAutoValueConverter) {
-		this.useAutoValueConverter = useAutoValueConverter;
-	}
+        this.useAutoValueConverter = useAutoValueConverter;
+    }
 }
