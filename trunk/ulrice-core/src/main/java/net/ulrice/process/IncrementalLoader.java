4c292cbd20112fb7066bc0f0475bfb920f2001e2
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2011-09-25 09:34:13 +0000

    Added incremental loader Changed translator processes to incremental loader

---
+++ trunk/ulrice-core/src/main/java/net/ulrice/process/IncrementalLoader.java
@@ -0,0 +1,65 @@
+package net.ulrice.process;
+
+import java.util.List;
+
+import net.ulrice.module.IFController;
+
+
+/**
+ * This class loads data incrementally in a background thread. (replacement for N4IncrementalLoadWorker)
+ * 
+ * new IncrementalLoader (new IncrementalDataProvoder <AreaFullTO> () {
+ *   public int getNumRows() {
+ *     return areaService.getNumAreas (salesCompany);
+ *   }
+ *   ...
+ * }
+ * 
+ * @author ehaasec
+ */
+public class IncrementalLoader<T> extends AbstractProcess<T, Object>{
+
+    //TODO cancellation support?
+    private final int chunkSize;
+    private final int upperBound;
+    private final IncrementalDataProvider<T> provider;
+    
+    private int totalNumRows;
+    private int numLoaded = 0;
+    
+    public IncrementalLoader (IFController owner, boolean blockController, IncrementalDataProvider<T> provider) {
+        this (owner, blockController, 100, Integer.MAX_VALUE, provider);
+    }
+        
+    public IncrementalLoader (IFController owner, boolean blockController, int chunkSize, int upperBound, IncrementalDataProvider<T> provider) {
+        super(owner, blockController);
+        this.chunkSize = chunkSize;
+        this.upperBound = upperBound;
+        this.provider = provider;
+    }
+    
+    @Override
+    protected T work() throws Exception {
+        totalNumRows = provider.getNumRows();
+        final int max = Math.min (upperBound, totalNumRows);
+        
+        while (numLoaded < max) {
+            final List<T> chunk = provider.getData (numLoaded, chunkSize);
+            provider.onChunkLoaded (chunk, numLoaded);
+            numLoaded += chunk.size();
+        }
+        provider.onFinished();
+        return null;
+    }
+
+    @Override
+    protected void finished(T result) {
+        //TODO cleanup?
+    }
+}
+
+
+
+
+
+
