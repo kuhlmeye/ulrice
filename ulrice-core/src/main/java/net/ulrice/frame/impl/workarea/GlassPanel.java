dc435181b7afd61dd71d38f7d597683cbd1cc088
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com>
2011-09-30 07:01:04 +0000

    Fixed glasspane

--- ulrice-core/src/main/java/net/ulrice/frame/impl/workarea/GlassPanel.java
+++ ulrice-core/src/main/java/net/ulrice/frame/impl/workarea/GlassPanel.java
@@ -2,80 +2,111 @@ package net.ulrice.frame.impl.workarea;
 
 import java.awt.AWTEvent;
 import java.awt.Component;
-import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.AWTEventListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.util.logging.Logger;
 
+import javax.swing.JComponent;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 import net.ulrice.ui.UI;
 
-public class GlassPanel extends JPanel implements AWTEventListener {
+public class GlassPanel extends JLayeredPane implements AWTEventListener, MouseWheelListener {
 
-	/** Default generated serial version uid. */
-	private static final long serialVersionUID = 382926138153043911L;
+    /** Default generated serial version uid. */
+    private static final long serialVersionUID = 382926138153043911L;
+       
+    private boolean blocked = false;
+   
 
-	/** The logger used by this class. */
-	private static final Logger LOG = Logger.getLogger(GlassPanel.class.getName());
+    private final JPanel overlayPanel = new JPanel() {
 
-	private boolean blocked = false;
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            setOpaque(false);
+            g.setColor(UIManager.getColor(UI.GLASSPANEL_COLOR));
+            g.fillRect(0, 0, getWidth(), getHeight());
+            g.dispose();
+        }
+    };
 
-	public boolean isBlocked() {
-		return blocked;
-	}
+    private JComponent view;
+    
+    public boolean isBlocked() {
+        return blocked;
+    }
 
-	public void setBlocked(boolean blocked) {
-		this.blocked = blocked;
-		if (blocked) {
-			Toolkit.getDefaultToolkit().addAWTEventListener(this, 0xFFF);
-		} else {
-			Toolkit.getDefaultToolkit().removeAWTEventListener(this);
-		}
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+        if (blocked) {
+            add(overlayPanel, new Integer(1));
+            overlayPanel.requestFocus();
+            overlayPanel.addMouseWheelListener(this);
+            Toolkit.getDefaultToolkit().addAWTEventListener(this, 0xFFF);
+        }
+        else {
+            remove(overlayPanel);
+            overlayPanel.removeMouseWheelListener(this);
+            Toolkit.getDefaultToolkit().removeAWTEventListener(this);
+        }
 
-		revalidate();
-		repaint();
-	}
+        doLayout();
+        revalidate();
+        repaint();
+    }
 
-	@Override
-	public void paint(Graphics g) {
-		if (blocked) {
-			setOpaque(false);
-			super.paint(g);
-			g.setColor(UIManager.getColor(UI.GLASSPANEL_COLOR));
-			g.fillRect(0, 0, getWidth(), getHeight());
-			g.dispose();
-			//TODO Wie wird der Cursor zurückgesetzt?
-			//setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-		} else {
-			super.paint(g);
-		}
-	}
+    public void addModuleView(JComponent view) {
+        this.view = view;
+        add(view, new Integer(0));
+    }
 
-	@Override
-	public void eventDispatched(AWTEvent event) {
-		if (event.getSource() instanceof Component) {
-			Component c = ((Component) event.getSource());
 
-			while (c.getParent() != null && c != this) {
-				c = c.getParent();
-			}
+    @Override
+    public void eventDispatched(AWTEvent event) {
+        if (event.getSource() instanceof Component) {
+            Component c = ((Component) event.getSource());
 
-			if ((event instanceof KeyEvent) && (event.getSource() instanceof Component)) {
-				if (c == this) {
-					((KeyEvent) event).consume();
-					LOG.fine("Key event consumed by blocked workarea");
-				}
-			} else if ((event instanceof MouseEvent) && (event.getSource() instanceof Component)) {
-				if (c == this) {
-					((MouseEvent) event).consume();
-				}
-			}
-		}
+            while (c.getParent() != null && c != this) {
+                c = c.getParent();
+            }
 
-	}
+            if ((event instanceof KeyEvent) && (event.getSource() instanceof Component)) {
+                if (c == this) {
+                    ((KeyEvent) event).consume();
+                }
+            }
+            else if ((event instanceof MouseEvent) && (event.getSource() instanceof Component)) {
+                if (c == this) {
+                    ((MouseEvent) event).consume();
+                }
+            }
+            else if ((event instanceof MouseWheelEvent) && (event.getSource() instanceof Component)) {
+                if (c == this) {
+                    ((MouseWheelEvent) event).consume();
+                }
+            }
+            
+        }
+
+    }
+
+    @Override
+    public void setBounds(int x, int y, int width, int height) {
+        super.setBounds(x, y, width, height);
+        view.setBounds(0, 0, width, height);
+        overlayPanel.setBounds(0, 0, width, height);
+    }
+
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        e.consume();
+    }
 }
