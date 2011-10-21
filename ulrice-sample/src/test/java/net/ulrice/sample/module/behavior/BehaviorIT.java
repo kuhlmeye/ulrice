eec351f13c31a3ae99475976bfb4bcaf25cc2b84
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com>
2011-10-21 13:55:40 +0000

    cuke4duke sux, long live cucumber-jvm

---
+++ ulrice-sample/src/test/java/net/ulrice/sample/module/behavior/BehaviorIT.java
@@ -0,0 +1,12 @@
+package net.ulrice.sample.module.behavior;
+
+import org.junit.runner.RunWith;
+
+import cucumber.junit.Cucumber;
+import cucumber.junit.Feature;
+
+@RunWith(Cucumber.class)
+@Feature(value = "net/ulrice/sample/module/behavior/behavior.feature")
+public class BehaviorIT {
+
+}
