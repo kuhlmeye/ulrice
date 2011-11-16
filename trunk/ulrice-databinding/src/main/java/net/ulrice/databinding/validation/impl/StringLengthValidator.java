52d48a55892f04d9f390dd349d5a91c58b1d24c8
apunahassaphemapetilon@hotmail.com <apunahassaphemapetilon@hotmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2011-11-16 09:44:43 +0000

    StringLengthValidator only validates string length now, additional NotNullValidator added

--- trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/validation/impl/StringLengthValidator.java
+++ trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/validation/impl/StringLengthValidator.java
@@ -19,11 +19,16 @@ public class StringLengthValidator extends AbstractValidator<String> {
 	protected ValidationResult validate(IFBinding bindingId, String attribute) {
 		ValidationResult result = new ValidationResult();
 		
-        if (attribute == null || attribute.length () < _minLength) {
+		// just validate length, not null should be handled by a different validator
+		if (attribute == null) {
+			return result;
+		}
+		
+        if (attribute.length () < _minLength) {
         	// TODO Christof Internationalize
             result.addValidationError(new ValidationError(bindingId, "min. Länge: " + _minLength, null));
         }
-        if (attribute == null || attribute.length () > _maxLength) {
+        if (attribute.length () > _maxLength) {
         	// TODO Christof Internationalize
             result.addValidationError(new ValidationError(bindingId, "max. Länge: " + _minLength, null));
         }
