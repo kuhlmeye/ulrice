64e2ddceb82be098936e8702ffbe496f2a96890f
apunahassaphemapetilon@hotmail.com <apunahassaphemapetilon@hotmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2011-11-17 11:26:29 +0000

    added UIManager to databinding -> BorderStateMarker now uses UIManager

--- ulrice-databinding/src/main/java/net/ulrice/databinding/ui/BindingUI.java
+++ ulrice-databinding/src/main/java/net/ulrice/databinding/ui/BindingUI.java
@@ -1,23 +1,25 @@
 package net.ulrice.databinding.ui;
 
 import java.awt.Color;
+import java.awt.Insets;
 
 import javax.swing.UIManager;
 
-public class BindingUI {
+import net.ulrice.databinding.viewadapter.impl.BorderStateMarker;
 
-	public static final String BACKGROUND_STATE_MARKER_INVALID = "Ulrice.Binding.BackgroundStateMarker.InvalidBackground";
-	public static final String BACKGROUND_STATE_MARKER_CHANGED = "Ulrice.Binding.BackgroundStateMarker.ChangedBackground";
+public class BindingUI implements BindingUIConstants {
 
-	public static final String BACKGROUND_NORMAL_ODD_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.NormalOddBackground";
-	public static final String BACKGROUND_NORMAL_EVEN_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.NormalOddBackground";
-	public static final String BACKGROUND_READONLY_ODD_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.ReadonlyOddBackground";
-	public static final String BACKGROUND_READONLY_EVEN_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.ReadonlyOddBackground";
-    public static final String BACKGROUND_SELECTED_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.SelectedBackground";
-    
-    public static final String MARKABLE_DURING_DISABLED_STATE  = "Ulrice.Binding.ViewAdapter.MarkableDuringDisabledState";
-    public static final String DISABLED_FOREGROUND = "Ulrice.Binding.ViewAdapter.DisabledForeground";
-    public static final String DISABLED_BACKGROUND = "Ulrice.Binding.ViewAdapter.DisabledBackground";
+	public static void applyDefaultUI() {
+		UIManager.put(BORDER_STATE_MARKER_INSETS, new Insets(2, 3, 2, 3));
+		UIManager.put(BORDER_STATE_MARKER_NORMAL_INNER_BORDER, Color.DARK_GRAY);
+		UIManager.put(BORDER_STATE_MARKER_NORMAL_OUTER_BORDER, Color.LIGHT_GRAY);
+		UIManager.put(BORDER_STATE_MARKER_CHANGED_INNER_BORDER, new Color(130, 130, 30));
+		UIManager.put(BORDER_STATE_MARKER_CHANGED_OUTER_BORDER, Color.LIGHT_GRAY);
+		UIManager.put(BORDER_STATE_MARKER_INVALID_INNER_BORDER, new Color(100, 30, 30));
+		UIManager.put(BORDER_STATE_MARKER_INVALID_OUTER_BORDER, Color.LIGHT_GRAY);
+		UIManager.put(BORDER_STATE_MARKER_CHANGED_IMAGE, BorderStateMarker.class.getResource("attention.png"));
+		UIManager.put(BORDER_STATE_MARKER_INVALID_IMAGE, BorderStateMarker.class.getResource("cross.png"));
+	}
 	
 	public static Color getColor(String key, Color defaultColor) {
 		Color color = UIManager.getColor(key);
---
+++ ulrice-databinding/src/main/java/net/ulrice/databinding/ui/BindingUIConstants.java
@@ -0,0 +1,27 @@
+package net.ulrice.databinding.ui;
+
+public interface BindingUIConstants {
+	
+	public static final String BORDER_STATE_MARKER_INSETS = "Ulrice.Binding.BorderStateMarker.Insets";
+	public static final String BORDER_STATE_MARKER_CHANGED_IMAGE = "Ulrice.Binding.BorderStateMarker.Image.Changed";
+	public static final String BORDER_STATE_MARKER_INVALID_IMAGE = "Ulrice.Binding.BorderStateMarker.Image.Invalid";
+	public static final String BORDER_STATE_MARKER_NORMAL_INNER_BORDER = "Ulrice.Binding.BorderStateMarker.Border.Inner.Normal";
+	public static final String BORDER_STATE_MARKER_NORMAL_OUTER_BORDER = "Ulrice.Binding.BorderStateMarker.Border.Outer.Normal";
+	public static final String BORDER_STATE_MARKER_CHANGED_INNER_BORDER = "Ulrice.Binding.BorderStateMarker.Border.Inner.Changed";
+	public static final String BORDER_STATE_MARKER_CHANGED_OUTER_BORDER = "Ulrice.Binding.BorderStateMarker.Border.Outer.Changed";
+	public static final String BORDER_STATE_MARKER_INVALID_INNER_BORDER = "Ulrice.Binding.BorderStateMarker.Border.Inner.Invalid";
+	public static final String BORDER_STATE_MARKER_INVALID_OUTER_BORDER = "Ulrice.Binding.BorderStateMarker.Border.Outer.Invalid";
+	public static final String BACKGROUND_STATE_MARKER_INVALID = "Ulrice.Binding.BackgroundStateMarker.InvalidBackground";
+	public static final String BACKGROUND_STATE_MARKER_CHANGED = "Ulrice.Binding.BackgroundStateMarker.ChangedBackground";
+
+	public static final String BACKGROUND_NORMAL_ODD_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.NormalOddBackground";
+	public static final String BACKGROUND_NORMAL_EVEN_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.NormalOddBackground";
+	public static final String BACKGROUND_READONLY_ODD_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.ReadonlyOddBackground";
+	public static final String BACKGROUND_READONLY_EVEN_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.ReadonlyOddBackground";
+    public static final String BACKGROUND_SELECTED_TABLE_ROW = "Ulrice.Binding.JTableVARenderer.SelectedBackground";
+    
+    public static final String MARKABLE_DURING_DISABLED_STATE  = "Ulrice.Binding.ViewAdapter.MarkableDuringDisabledState";
+    public static final String DISABLED_FOREGROUND = "Ulrice.Binding.ViewAdapter.DisabledForeground";
+    public static final String DISABLED_BACKGROUND = "Ulrice.Binding.ViewAdapter.DisabledBackground";
+
+}
