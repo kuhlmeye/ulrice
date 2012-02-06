d7966cb22980a1fd7be0fc1b21b07b6ee9c8d809
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com>
2012-02-06 12:46:45 +0000

    implemented enter method for JListHelper (remote-control)

--- ulrice-remote-control/src/main/java/net/ulrice/remotecontrol/impl/helper/JCheckBoxHelper.java
+++ ulrice-remote-control/src/main/java/net/ulrice/remotecontrol/impl/helper/JCheckBoxHelper.java
@@ -5,8 +5,6 @@ import java.awt.Robot;
 import javax.swing.JCheckBox;
 
 import net.ulrice.remotecontrol.RemoteControlException;
-import net.ulrice.remotecontrol.util.RemoteControlUtils;
-import net.ulrice.remotecontrol.util.Result;
 
 public class JCheckBoxHelper extends AbstractJComponentHelper<JCheckBox> {
 
@@ -17,28 +15,14 @@ public class JCheckBoxHelper extends AbstractJComponentHelper<JCheckBox> {
 
     @Override
     public boolean enter(Robot robot, final JCheckBox component, String text) throws RemoteControlException {
-        final Result<Boolean> result = new Result<Boolean>(1);
         final boolean valueToSet = Boolean.parseBoolean(text);
         final boolean currentValue = component.isSelected();
-        
+
         if (valueToSet != currentValue) {
             return click(robot, component);
         }
-        
-        return true;
 
-//       
-//        RemoteControlUtils.invokeInSwing(new Runnable() {
-//
-//            @Override
-//            public void run() {
-//                component.setSelected(valueToSet);
-//                result.fireResult(true);
-//            }
-//
-//        });
-//
-//        return result.aquireResult();
+        return true;
     }
 
     @Override
