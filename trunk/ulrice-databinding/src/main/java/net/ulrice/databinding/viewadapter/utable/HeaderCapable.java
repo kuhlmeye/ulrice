5cf5998e7180aabc382e7f3c86902f0834e6bd5e
ibolyagb@gmail.com <ibolyagb@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2012-02-20 10:07:00 +0000

    row collapse by header

---
+++ trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/HeaderCapable.java
@@ -0,0 +1,10 @@
+package net.ulrice.databinding.viewadapter.utable;
+
+public interface HeaderCapable {
+
+    /**
+     * Header row flag for collapse rows with UTableVAFilter
+     */
+    public abstract boolean isHeader();
+
+}
