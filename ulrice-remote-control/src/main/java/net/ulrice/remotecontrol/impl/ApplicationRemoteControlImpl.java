a945ef38ed4e328d1ad9ae0689c29eb4a6b61977
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com>
2012-03-15 08:57:17 +0000

    added screenshot option to applicationremotecontrol

--- ulrice-remote-control/src/main/java/net/ulrice/remotecontrol/impl/ApplicationRemoteControlImpl.java
+++ ulrice-remote-control/src/main/java/net/ulrice/remotecontrol/impl/ApplicationRemoteControlImpl.java
@@ -1,6 +1,18 @@
 package net.ulrice.remotecontrol.impl;
 
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Window;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import net.ulrice.remotecontrol.ApplicationRemoteControl;
+import net.ulrice.remotecontrol.RemoteControlException;
+import net.ulrice.remotecontrol.util.ComponentUtils;
 
 /**
  * Implementation of the {@link ApplicationRemoteControl}
@@ -29,4 +41,49 @@ public class ApplicationRemoteControlImpl implements ApplicationRemoteControl {
         System.exit(0);
     }
 
+    @Override
+    public byte[] screenshot() throws RemoteControlException {
+        Window[] windows = Window.getWindows();
+        Rectangle rectangle = null;
+
+        for (Window window : windows) {
+            if (window.isVisible()) {
+                Rectangle bounds = window.getBounds();
+
+                rectangle = (rectangle != null) ? bounds.union(rectangle) : bounds;
+
+                ComponentUtils.toFront(window);
+            }
+        }
+
+        Robot robot;
+
+        try {
+            robot = new Robot();
+        }
+        catch (AWTException e) {
+            throw new RemoteControlException("Failed to tell robot to capture screenshot", e);
+        }
+
+        BufferedImage screenshot = robot.createScreenCapture(rectangle);
+
+        ByteArrayOutputStream out;
+
+        try {
+            out = new ByteArrayOutputStream();
+
+            try {
+                ImageIO.write(screenshot, "PNG", out);
+
+            }
+            finally {
+                out.close();
+            }
+        }
+        catch (IOException e) {
+            throw new RemoteControlException("Failed to write screenshot", e);
+        }
+
+        return out.toByteArray();
+    }
 }
