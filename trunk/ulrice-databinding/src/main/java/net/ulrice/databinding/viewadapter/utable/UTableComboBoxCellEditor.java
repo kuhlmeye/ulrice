f311aaa8e3f97590f66bb9d510471cbc527c007f
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2012-03-18 14:40:20 +0000

    width of combobox popup is now independent from cell width

--- trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTableComboBoxCellEditor.java
+++ trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTableComboBoxCellEditor.java
@@ -1,6 +1,8 @@
 package net.ulrice.databinding.viewadapter.utable;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.util.EventObject;
 import java.util.List;
@@ -8,7 +10,12 @@ import java.util.List;
 import javax.swing.AbstractCellEditor;
 import javax.swing.AbstractListModel;
 import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
 import javax.swing.table.TableCellEditor;
 
 import net.ulrice.databinding.ObjectWithPresentation;
@@ -27,6 +34,70 @@ public class UTableComboBoxCellEditor extends AbstractCellEditor implements Tabl
     public UTableComboBoxCellEditor(JComboBox comboBox) {
         this.comboBox = comboBox;
         this.comboBox.setBorder(null);        
+        
+        this.comboBox.addPopupMenuListener(new PopupMenuListener() {
+
+            /**
+             * {@inheritDoc}
+             */
+            public void popupMenuCanceled(PopupMenuEvent e) {
+                // Synchronize the selected list item with the text field
+                JComboBox box = (JComboBox) e.getSource();
+                Object selectedItem = box.getSelectedItem();
+                box.getEditor().setItem(selectedItem);
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+                // Synchronize the selected list item with the text field
+                JComboBox box = (JComboBox) e.getSource();
+                Object selectedItem = box.getSelectedItem();
+                box.getEditor().setItem(selectedItem);
+            }
+
+            /**
+             * {@inheritDoc}
+             */
+            public void popupMenuWillBecomeVisible(PopupMenuEvent e) {
+
+                JComboBox box = (JComboBox) e.getSource();
+
+                // Get the popup menu.
+                Object comp = box.getUI().getAccessibleChild(box, 0);
+                if (!(comp instanceof JPopupMenu)) {
+                    return;
+                }
+                JPopupMenu popupMenu = (JPopupMenu) comp;
+
+                // Get the scrollpane in the scroller.
+                if (!(popupMenu.getComponent(0) instanceof JScrollPane)) {
+                    return;
+                }
+                JScrollPane scrollPane = (JScrollPane) ((JPopupMenu) comp).getComponent(0);
+                popupMenu.remove(scrollPane);
+                popupMenu.setLayout(new BorderLayout());
+                popupMenu.add(scrollPane, BorderLayout.CENTER);
+                // Get the component from the scrollpane.
+                if (!(scrollPane.getViewport().getView() instanceof JComponent)) {
+                    return;
+                }
+                JComponent comboBoxPopup = (JComponent) scrollPane.getViewport().getView();
+
+                // Check the size of the popup and set it to preferred size, if
+                // the size
+                // is greater than the size of the combo box.
+                Dimension size = new Dimension();
+                size.width =
+                        comboBoxPopup.getPreferredSize().width < scrollPane.getPreferredSize().width ? scrollPane
+                            .getPreferredSize().width : comboBoxPopup.getPreferredSize().width + 15;
+                size.height = scrollPane.getPreferredSize().height;
+                scrollPane.setPreferredSize(size);
+                scrollPane.setMinimumSize(size);
+                scrollPane.setMaximumSize(size);
+            }
+        });
     }
 
     public UTableComboBoxCellEditor(List< ?> valueRange) {
