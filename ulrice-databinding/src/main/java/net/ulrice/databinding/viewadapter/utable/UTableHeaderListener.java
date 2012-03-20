f4e9158f6cf00c7a31f533fe08f0cc326a69766e
ibolyagb@gmail.com <ibolyagb@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2012-03-20 14:47:23 +0000

    Mandatory sort key handling

--- ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTableHeaderListener.java
+++ ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTableHeaderListener.java
@@ -52,8 +52,12 @@ public class UTableHeaderListener implements MouseListener, MouseMotionListener 
          
         UTableRowSorter sorter = uTable.getRowSorter();  table.getRowSorter();
         List< ? extends SortKey> oldKeys = sorter.getGlobalSortKeys();
-        
-        List<SortKey> newKeys = new ArrayList<SortKey>();        
+
+        List<SortKey> newKeys = new ArrayList<SortKey>();
+        boolean hasMandatorySortKeys = sorter.getMandatorySortKeys() != null && !sorter.getMandatorySortKeys().isEmpty(); 
+        if(!multi && hasMandatorySortKeys){
+            newKeys.addAll(sorter.getMandatorySortKeys());
+        }
         boolean found = false;
         for (SortKey k : oldKeys) {
             if (k.getColumn() == colModel) {
@@ -64,7 +68,13 @@ public class UTableHeaderListener implements MouseListener, MouseMotionListener 
                         newOrder = SortOrder.DESCENDING;
                         break;
                     case DESCENDING:
-                        newOrder = SortOrder.UNSORTED;
+                        if (hasMandatorySortKeys
+                            && UTableRowSorter.containsSortKey(sorter.getMandatorySortKeys(), k.getColumn())) {
+                            newOrder = SortOrder.DESCENDING;
+                        }
+                        else {
+                            newOrder = SortOrder.UNSORTED;
+                        }
                         break;
                     case UNSORTED:
                         newOrder = SortOrder.ASCENDING;
