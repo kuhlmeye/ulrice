f3d8ffa7b6b770729d25d56bfd352390455c40f2
andreas.radauer@gmail.com <andreas.radauer@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2012-04-25 17:32:18 +0000

    #3178: tree table fixes

--- trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTableVAHeaderRenderer.java
+++ trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTableVAHeaderRenderer.java
@@ -14,11 +14,12 @@ import net.ulrice.databinding.viewadapter.impl.JTableViewAdapter;
 
 /**
  * @author christof
- * 
  */
 public class UTableVAHeaderRenderer implements TableCellRenderer {
 
     private TableCellRenderer labelRenderer;
+
+    private ExpandColapsePanel expandColapsePanel;
 
     /**
      * @param tableGA
@@ -29,25 +30,39 @@ public class UTableVAHeaderRenderer implements TableCellRenderer {
     }
 
     /**
-     * @see javax.swing.table.TableCellRenderer#getTableCellRendererComponent(javax.swing.JTable,
-     *      java.lang.Object, boolean, boolean, int, int)
+     * @see javax.swing.table.TableCellRenderer#getTableCellRendererComponent(javax.swing.JTable, java.lang.Object,
+     *      boolean, boolean, int, int)
      */
     @Override
     public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-            int row, int column) {
+        int row, int column) {
 
         Object renderValue = value;
         String tooltipText = null;
-        if (value instanceof ColumnDefinition<?>) {
-            ColumnDefinition< ?> columnDefinition = (ColumnDefinition<?>) value;
+        if (value instanceof ColumnDefinition< ?>) {
+            ColumnDefinition< ?> columnDefinition = (ColumnDefinition< ?>) value;
             renderValue = columnDefinition.getColumnName();
             tooltipText = columnDefinition.getColumnTooltip();
         }
-        
-        
-        Component component = labelRenderer.getTableCellRendererComponent(table, renderValue, isSelected, hasFocus, row, column);
-        if(JComponent.class.isAssignableFrom(JComponent.class)) {
-            ((JComponent)component).setToolTipText(tooltipText);
+
+        Component component =
+                labelRenderer.getTableCellRendererComponent(table, renderValue, isSelected, hasFocus, row, column);
+        if (JComponent.class.isAssignableFrom(JComponent.class)) {
+            ((JComponent) component).setToolTipText(tooltipText);
+        }
+
+        if (column == 0) {
+            if (table instanceof UTable) {
+                UTable uTable = (UTable) table;
+                if (uTable.getTableComponent() instanceof UTreeTableComponent) {
+                    UTreeTableComponent treeTable = (UTreeTableComponent) uTable.getTableComponent();
+                    if (expandColapsePanel == null) {
+                        expandColapsePanel = new ExpandColapsePanel(treeTable);
+                    }
+                    component = expandColapsePanel;
+                }
+
+            }
         }
         return component;
     }
