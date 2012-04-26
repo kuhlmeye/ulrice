c4339704aa6c56c85851766177daa6c58b0e8e9e
rene.zimmermann.wob@googlemail.com <rene.zimmermann.wob@googlemail.com>
2012-04-26 11:43:05 +0000

    Validate minimum and maximum value of an Integer, not the length.

--- ulrice-databinding/src/main/java/net/ulrice/databinding/validation/impl/IntegerLengthValidator.java
+++ ulrice-databinding/src/main/java/net/ulrice/databinding/validation/impl/IntegerLengthValidator.java
@@ -1,28 +1,42 @@
 package net.ulrice.databinding.validation.impl;
 
 /**
- * Save while using StringToIntegerConverter.
+ * Integer validator checking the minimum and maximum value of an Integer.
  * 
  * @author - apunahassaphemapetilon@hotmail.com
  */
 import net.ulrice.databinding.IFBinding;
 import net.ulrice.databinding.validation.AbstractValidator;
+import net.ulrice.databinding.validation.ValidationError;
 import net.ulrice.databinding.validation.ValidationResult;
 
 public class IntegerLengthValidator extends AbstractValidator<Integer> {
     
-    private final StringLengthValidator validator;
+    private final Integer minValue;
+    private final Integer maxValue;
     
-    public IntegerLengthValidator (Integer minLength, Integer maxLength) {
-        validator = new StringLengthValidator(minLength, maxLength);
+    public IntegerLengthValidator (Integer minValue, Integer maxValue) {
+        this.minValue = minValue;
+        this.maxValue = maxValue;
     }
 
     @Override
     protected ValidationResult validate(IFBinding bindingId, Integer attribute) {
         ValidationResult result = new ValidationResult();
-        if (attribute != null) {
-            result = validator.validate(bindingId, attribute.toString());
+        
+        if (attribute == null) {
+            return result;
         }
+        
+        if (minValue != null && attribute < minValue) {
+            // TODO Christof Internationalize
+            result.addValidationError(new ValidationError(bindingId, "min. Wert: " + minValue, null));
+        }
+        if (maxValue != null && attribute > maxValue) {
+            // TODO Christof Internationalize
+            result.addValidationError(new ValidationError(bindingId, "max. Wert: " + maxValue, null));
+        }
+        
         return result;
     }
 
