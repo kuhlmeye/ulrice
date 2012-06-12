4c0b3fb0d0e7108fe10a5c6a428714762ecf9640
dahu4wa@gmail.com <dahu4wa@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2012-06-12 06:07:49 +0000

    helper for tabbed pane in BDD

--- trunk/ulrice-remote-control/src/main/java/net/ulrice/remotecontrol/impl/helper/JTabbedPaneHelper.java
+++ trunk/ulrice-remote-control/src/main/java/net/ulrice/remotecontrol/impl/helper/JTabbedPaneHelper.java
@@ -1,40 +1,75 @@
 package net.ulrice.remotecontrol.impl.helper;
 
 import java.awt.Component;
+import java.awt.Robot;
 
 import javax.swing.JTabbedPane;
 
 import net.ulrice.remotecontrol.ComponentListData;
 import net.ulrice.remotecontrol.RemoteControlException;
 
-public class JTabbedPaneHelper extends AbstractJComponentHelper<JTabbedPane>
-{
+public class JTabbedPaneHelper extends AbstractJComponentHelper<JTabbedPane> {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Class<JTabbedPane> getType()
-	{
-		return JTabbedPane.class;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Class<JTabbedPane> getType() {
+        return JTabbedPane.class;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Object getData(JTabbedPane component) throws RemoteControlException
-	{
-		ComponentListData result = new ComponentListData();
+    private int indexOfTab(JTabbedPane component, String title) {
+        for (int i=0; i<component.getTabCount(); i+=1) {
+            if (title.equals(component.getTitleAt(i))) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+    
+    /**
+     * {@inheritDoc}
+     * @see net.ulrice.remotecontrol.impl.helper.AbstractComponentHelper#click(java.awt.Robot, java.awt.Component, java.lang.String)
+     */
+    @Override
+    public boolean click(Robot robot, JTabbedPane component, String text) throws RemoteControlException {
+        int index = indexOfTab(component, text);
+        
+        if (index < 0) {
+            return false;
+        }
+        
+        return click(robot, component, index);
+    }
 
-		for (int i = 0; i < component.getTabCount(); i += 1)
-		{
-			Component tabComponentAt = component.getTabComponentAt(i);
-			
+    /**
+     * {@inheritDoc}
+     * @see net.ulrice.remotecontrol.impl.helper.AbstractComponentHelper#click(java.awt.Robot, java.awt.Component, int)
+     */
+    @Override
+    public boolean click(Robot robot, JTabbedPane component, int index) throws RemoteControlException {
+        if (index >= component.getTabCount()) {
+            return false;
+        }
+
+        return super.click(robot, component, component.getBoundsAt(index));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Object getData(JTabbedPane component) throws RemoteControlException {
+        ComponentListData result = new ComponentListData();
+
+        for (int i = 0; i < component.getTabCount(); i += 1) {
+            Component tabComponentAt = component.getTabComponentAt(i);
+
             result.addEntry((tabComponentAt != null) ? tabComponentAt.getClass().getName() : Void.class, component.getSelectedIndex() == i);
-		}
+        }
 
-		return result;
-	}
+        return result;
+    }
 
 }
