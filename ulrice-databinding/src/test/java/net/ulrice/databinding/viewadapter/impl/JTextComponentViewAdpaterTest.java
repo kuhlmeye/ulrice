96d1c3b5005695560ff9fe9f9b6b84f0fcc627f9
andreas.hud@googlemail.com <andreas.hud@googlemail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2012-06-12 14:03:43 +0000

    Added tests for JTextComponentViewAdapter

---
+++ ulrice-databinding/src/test/java/net/ulrice/databinding/viewadapter/impl/JTextComponentViewAdpaterTest.java
@@ -0,0 +1,31 @@
+package net.ulrice.databinding.viewadapter.impl;
+
+import javax.swing.JTextField;
+
+import junit.framework.TestCase;
+
+public class JTextComponentViewAdpaterTest extends TestCase {
+    
+    public void testSetComponentEnabled() {
+        JTextField textField = new JTextField();
+        JTextComponentViewAdapter textVA = new JTextComponentViewAdapter(textField);
+        
+        textVA.setEnableSelectionIfComponentDisabled(true);
+        textVA.setComponentEnabled(false);
+        
+        //Is this even possible? JavaDoc of component.setEnabled says that a component
+        //is not editable after a call of setEnabled(false). Vice versa?
+        assertTrue(textField.isEnabled());
+        assertFalse(textField.isEditable());
+    }
+    
+    public void testSetComponentEditable() {
+        JTextField textField = new JTextField();
+        JTextComponentViewAdapter textVA = new JTextComponentViewAdapter(textField);
+        
+        textVA.setEditable(false);
+        
+        assertFalse(textField.isEditable());
+    }
+    
+}
