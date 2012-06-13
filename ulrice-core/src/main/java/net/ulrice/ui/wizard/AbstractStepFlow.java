1438c5837a8d7cdc44891ea7f4299dd4a37e49da
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com>
2012-06-13 05:57:49 +0000

    Add conditional flows to wizard

--- ulrice-core/src/main/java/net/ulrice/ui/wizard/AbstractStepFlow.java
+++ ulrice-core/src/main/java/net/ulrice/ui/wizard/AbstractStepFlow.java
@@ -1,89 +1,26 @@
 package net.ulrice.ui.wizard;
 
-import java.awt.event.ActionEvent;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.swing.AbstractAction;
-import javax.swing.Action;
-import javax.swing.event.EventListenerList;
 
 public abstract class AbstractStepFlow implements StepFlow {
 
-    private EventListenerList listenerList = new EventListenerList();        
-    private Map<String, Step> steps = new HashMap<String, Step>();
-    private Action prevAction = new AbstractAction() {
-        private static final long serialVersionUID = 1L;
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            prev();
-        }        
-    };
-    private Action nextAction = new AbstractAction() {
-        private static final long serialVersionUID = 1L;
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            next();
-        }        
-    };
-    
+    private WizardData data = new WizardData();
+    private Wizard wizard;
 
     @Override
-    public void addStepFlowEventListener(StepFlowEventListener listener) {
-        listenerList.add(StepFlowEventListener.class, listener);
+    public void initialize(Wizard wizard) {
+        this.wizard = wizard;
     }
     
-    @Override
-    public void removeStepFlowEventListener(StepFlowEventListener listener) {
-        listenerList.remove(StepFlowEventListener.class, listener);
-    }    
-    
-    @Override
-    public void addStep(Step step) {
-        steps.put(step.getId(), step);
-        stepAdded(step);
-        
-        fireStepFlowChanged();
-    }
-
-    protected abstract void stepAdded(Step step);
-
-    @Override
-    public void delStep(Step step) {
-        steps.remove(step.getId());
-        stepRemoved(step);
-    }
-
-    protected abstract void stepRemoved(Step step);
-
-    @Override
-    public Step getStepById(String id) {
-        return steps.get(id);
-    }
-    
-    private void fireStepFlowChanged() {
-        StepFlowEventListener[] listeners = listenerList.getListeners(StepFlowEventListener.class);
-        for(StepFlowEventListener listener : listeners) {
-            listener.stepFlowChanged(this);
-        }
+    protected void fireStepFlowChanged() {
+        wizard.stepFlowChanged(this);
     }
     
     protected void fireCurrentStepChanged() {
-        StepFlowEventListener[] listeners = listenerList.getListeners(StepFlowEventListener.class);
-        for(StepFlowEventListener listener : listeners) {
-            listener.currentStepChanged(this);
-        }
+        wizard.currentStepChanged(this);
     }
     
     @Override
-    public Action getPrevAction() {
-        return prevAction;
-    }
-    
-    @Override
-    public Action getNextAction() {
-        return nextAction;
+    public WizardData getWizardData() {
+        return data;
     }
 }
