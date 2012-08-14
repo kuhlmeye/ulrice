164336fd0192d709ec2a94304f547eabb312b96b
andreas.radauer@gmail.com <andreas.radauer@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2012-08-14 07:35:54 +0000

    improved color override

--- trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/impl/BackgroundStateMarker.java
+++ trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/impl/BackgroundStateMarker.java
@@ -4,23 +4,25 @@ import java.awt.Color;
 
 import javax.swing.JComponent;
 
+import net.ulrice.databinding.bufferedbinding.impl.ColumnColorOverride;
 import net.ulrice.databinding.bufferedbinding.impl.Element;
 import net.ulrice.databinding.ui.BindingUI;
 import net.ulrice.databinding.viewadapter.IFCellStateMarker;
 import net.ulrice.databinding.viewadapter.IFStateMarker;
+import net.ulrice.util.Colors;
 
 public class BackgroundStateMarker implements IFStateMarker, IFCellStateMarker {
 
     private static final Color evenNormalBackground = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_NORMAL_EVEN, new Color(230, 230, 230));
-    private static final Color oddNormalBackground = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_NORMAL_ODD, new Color(200,200, 200));
-    private static final Color evenReadOnlyBackground = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_READONLY_EVEN,new Color(200, 230, 200));
+    private static final Color oddNormalBackground = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_NORMAL_ODD, new Color(200, 200, 200));
+    private static final Color evenReadOnlyBackground = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_READONLY_EVEN, new Color(200, 230, 200));
     private static final Color oddReadOnlyBackground = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_READONLY_ODD, new Color(170, 200, 170));
     private static final Color selectedBackground = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_SELECTED, new Color(200, 200, 255));
-    
+
     private static final Color REMOVED_BG_COLOR = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_REMOVED_BG, new Color(150, 150, 150));
     private static final Color REMOVED_FG_COLOR = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_REMOVED_FG, new Color(80, 80, 80));
     private static final Color INVALID_BG_COLOR = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_INVALID, new Color(200, 130, 130));
-    private static final Color CHANGED_BG_COLOR = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_CHANGED, new Color(186,207,226));
+    private static final Color CHANGED_BG_COLOR = BindingUI.getColor(BindingUI.BACKGROUND_STATE_MARKER_CHANGED, new Color(186, 207, 226));
     private Color normalFGColor;
     private Color normalBGColor;
 
@@ -28,22 +30,24 @@ public class BackgroundStateMarker implements IFStateMarker, IFCellStateMarker {
     public void updateState(Object value, boolean editable, boolean dirty, boolean valid, JComponent component) {
         if (!valid) {
             component.setBackground(INVALID_BG_COLOR);
-        } else {
+        }
+        else {
             if (dirty) {
                 component.setBackground(CHANGED_BG_COLOR);
-            } else {
+            }
+            else {
                 component.setBackground(normalBGColor);
             }
         }
     }
 
     @Override
-    public void updateState(Element value, int row, boolean selected, boolean editable, boolean dirty, boolean valid,
-        JComponent component) {
+    public void updateState(Element value, int row, boolean selected, boolean editable, boolean dirty, boolean valid, ColumnColorOverride columnColorOverride, JComponent component) {
         if (value.isRemoved() && !selected) {
             component.setForeground(REMOVED_FG_COLOR);
             component.setBackground(REMOVED_BG_COLOR);
-        } else if (!valid) {
+        }
+        else if (!valid) {
             component.setForeground(normalFGColor);
             component.setBackground(INVALID_BG_COLOR);
         }
@@ -55,15 +59,25 @@ public class BackgroundStateMarker implements IFStateMarker, IFCellStateMarker {
             else {
                 component.setForeground(normalFGColor);
                 if (!editable) {
-                    component.setBackground(row % 2 == 0 ? evenReadOnlyBackground : oddReadOnlyBackground);
+                    if (row % 2 == 0) {
+                        component.setBackground(columnColorOverride == null ? evenReadOnlyBackground : columnColorOverride.getEvenReadOnlyColor());
+                    }
+                    else {
+                        component.setBackground(columnColorOverride == null ? oddReadOnlyBackground : columnColorOverride.getOddReadOnlyColor());
+                    }
                 }
                 else {
-                    component.setBackground(row % 2 == 0 ? evenNormalBackground : oddNormalBackground);
+                    if (row % 2 == 0) {
+                        component.setBackground(columnColorOverride == null ? evenNormalBackground : columnColorOverride.getEvenNormalColor());
+                    }
+                    else {
+                        component.setBackground(columnColorOverride == null ? oddNormalBackground : columnColorOverride.getOddNormalColor());
+                    }
                 }
             }
         }
         if (selected) {
-            component.setBackground(blend(component.getBackground(), selectedBackground, 0.5));
+            component.setBackground(makeColorSelected(component.getBackground()));
         }
     }
 
@@ -77,19 +91,13 @@ public class BackgroundStateMarker implements IFStateMarker, IFCellStateMarker {
         }
     }
 
-    private static Color blend(Color color1, Color color2, double ratio) {
-        float r = (float) ratio;
-        float ir = (float) 1.0 - r;
-
-        float rgb1[] = new float[3];
-        float rgb2[] = new float[3];
-
-        color1.getColorComponents(rgb1);
-        color2.getColorComponents(rgb2);
-
-        Color color = new Color(rgb1[0] * r + rgb2[0] * ir, rgb1[1] * r + rgb2[1] * ir, rgb1[2] * r + rgb2[2] * ir);
-
-        return color;
+    /**
+     * to get a consistend selection colour behavior TODO: description
+     */
+    public static Color makeColorSelected(Color color) {
+        return Colors.blend(color, selectedBackground, 0.5);
     }
+
+   
 
 }
