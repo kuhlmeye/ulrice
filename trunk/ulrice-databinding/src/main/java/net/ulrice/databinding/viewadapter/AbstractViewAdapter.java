9e10f2fe9e1508674156761660957893fe42f180
andreas.hud@googlemail.com <andreas.hud@googlemail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2012-09-28 15:48:50 +0000

    After calling genericAM.setReadOnly(true) a component will be compeletely disabled (grey and not usable). But after calling genericAM.setReadOnly(false) the components ViewAdapter will not be set editable(true) so the component still is grey and unusable. This commit will fix this behaviour.

--- trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/AbstractViewAdapter.java
+++ trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/AbstractViewAdapter.java
@@ -102,7 +102,7 @@ public abstract class AbstractViewAdapter<M, V> implements IFViewAdapter<M, V> {
             setReadOnlyBinding(binding.isReadOnly());
             setValue((M) binding.getCurrentValue());
 
-            setEditable(isEditable() && !binding.isReadOnly());
+            setEditable(!binding.isReadOnly());
             
             addComponentListener();
         }
