1ea75ad7ca59a57aa84c9f621e88f9f06dd39daf
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com>
2012-09-28 12:44:54 +0000

    added description to screenshots

--- ulrice-remote-control/src/main/java/net/ulrice/remotecontrol/ApplicationRemoteControl.java
+++ ulrice-remote-control/src/main/java/net/ulrice/remotecontrol/ApplicationRemoteControl.java
@@ -17,13 +17,15 @@ public interface ApplicationRemoteControl {
     /**
      * Captures a screen shot of all visible frames and creates a byte array containing the PNG image
      * 
+     * @param description some description, may be null
+     * @param failure TODO
      * @return a byte array containing a PNG image
      * @throws RemoteControlException on occasion
      */
-    public byte[] screenshot() throws RemoteControlException;
-    
+    public byte[] screenshot(String description, boolean failure) throws RemoteControlException;
+
     public void overrideSpeedFactor(double speedFactor);
-    
+
     public boolean combinedWaitFor(double timeoutInSeconds, ComponentMatcher componentMatcher, ControllerMatcher controllerMatcher) throws RemoteControlException;
 
 }
