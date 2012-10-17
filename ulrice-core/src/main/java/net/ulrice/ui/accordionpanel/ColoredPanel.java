ca72050e22069ba85346f80c4a455dab7b5a60e0
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com>
2012-10-17 15:23:09 +0000

    finished rules maintenance and BDD tests

--- ulrice-core/src/main/java/net/ulrice/ui/accordionpanel/ColoredPanel.java
+++ ulrice-core/src/main/java/net/ulrice/ui/accordionpanel/ColoredPanel.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.LinearGradientPaint;
+import java.awt.Paint;
 import java.awt.geom.Point2D;
 
 import javax.swing.JPanel;
@@ -18,44 +19,113 @@ import net.ulrice.util.Colors;
  * @author EXSTHUB, EEXHANT
  */
 public class ColoredPanel extends JPanel {
+
     private static final long serialVersionUID = 4952054987230225700L;
-    private static final Color WHITE_BLUE = new Color(0xecf4fb);
-    public static final Color LIGHT_BLUE = new Color(0xbed3e4);
+
+    public static interface PaintStrategy {
+
+        Paint getPaint(Color foreground, Color background, int height);
+
+    }
+
+    public static PaintStrategy RAISED_PAINT = new PaintStrategy() {
+
+        @Override
+        public Paint getPaint(Color foreground, Color background, int height) {
+            Color shadowColor = Colors.darker(background, 0.1);
+            Color contentColor = background;
+            Color darkerColor = Colors.darker(background, 0.08);
+
+            if (height > 15) { // better looking effect
+                return new LinearGradientPaint(new Point2D.Double(0, 0), new Point2D.Double(0, height), //
+                    new float[] { 0.0f, 0.01f, 1.0f - (12.0f / height), 1.0f }, //
+                    new Color[] { shadowColor, contentColor, contentColor, darkerColor });
+            }
+
+            // alternative effect, only uses relative values
+            return new LinearGradientPaint(new Point2D.Double(0, 0), new Point2D.Double(0, height), //
+                new float[] { 0.0f, 0.01f, 0.93f, 1.0f }, //
+                new Color[] { shadowColor, contentColor, contentColor, darkerColor });
+        }
+
+    };
+
+    public static PaintStrategy LOWERED_PAINT = new PaintStrategy() {
+
+        @Override
+        public Paint getPaint(Color foreground, Color background, int height) {
+            Color lineColor = Colors.darker(background, 0.2);
+            Color shadowColor = Colors.darker(background, 0.1);
+            Color contentColor = background;
+            float fractionA = (float) Math.min(1.0 / height, 0.25);
+            float fractionB = (float) Math.min(8.0 / height, 0.5);
+
+            return new LinearGradientPaint(new Point2D.Double(0, 0), new Point2D.Double(0, height), new float[] { 0.0f, fractionA, fractionB, 1.0f }, new Color[] {
+                lineColor, shadowColor, contentColor, contentColor });
+        }
+
+    };
+
+    public static ColoredPanel createRaised(JPanel content) {
+        return createRaised(null, content);
+    }
+
+    public static ColoredPanel createRaised(Color color, JPanel content) {
+        ColoredPanel result = new ColoredPanel(RAISED_PAINT, content);
+
+        if (color != null) {
+            result.setBackground(color);
+        }
+
+        return result;
+    }
+
+    public static ColoredPanel createLowered(JPanel content) {
+        return createLowered(new Color(0xe3e3e3), content);
+    }
+
+    public static ColoredPanel createLowered(Color color, JPanel content) {
+        ColoredPanel result = new ColoredPanel(LOWERED_PAINT, content);
+
+        if (color != null) {
+            result.setBackground(color);
+        }
+
+        return result;
+    }
+
+    private final PaintStrategy paintStrategy;
 
     public ColoredPanel(JPanel content) {
+        this(RAISED_PAINT, content);
+    }
+
+    public ColoredPanel(PaintStrategy paintStrategy, JPanel content) {
         super(new BorderLayout());
+
+        this.paintStrategy = paintStrategy;
 
         content.setOpaque(false);
 
         add(content);
-
+        setBackground(new Color(0xecf4fb));
         setOpaque(false);
-        setBackground(WHITE_BLUE);
         add(new JSeparator(), BorderLayout.SOUTH);
     }
 
+    /**
+     * TODO: description
+     * 
+     * @param g
+     */
+    @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        LinearGradientPaint paint = null;
 
-        //TODO maybe refactor
-        
-        if (getHeight() > 15) { //better looking effect
-            paint = new LinearGradientPaint(new Point2D.Double(0, 0), new Point2D.Double(0, getHeight()), //
-                new float[] { 0.0f, 0.01f, 1.0f - 12.0f / getHeight(), 1.0f }, //
-                new Color[] { LIGHT_BLUE, WHITE_BLUE, WHITE_BLUE, Colors.darker(getBackground(), 0.08) });
-        }
-        else { //alternative effect, only uses relative values
-            paint = new LinearGradientPaint(new Point2D.Double(0, 0), new Point2D.Double(0, getHeight()), //
-                new float[] { 0.0f, 0.01f, 0.93f, 1.0f }, //
-                new Color[] { LIGHT_BLUE, WHITE_BLUE, WHITE_BLUE, Colors.darker(getBackground(), 0.08) });
-        }
-
-        g2.setPaint(paint);
+        g2.setPaint(paintStrategy.getPaint(getForeground(), getBackground(), getHeight()));
         g2.fillRect(0, 0, getWidth(), getHeight());
 
         super.paintComponent(g);
-
     }
 
 }
