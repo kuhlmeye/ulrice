22f0d35d5e0d5fd8a31410e3125395af5160c88c
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com>
2012-11-30 16:07:21 +0000

    fixed some ui-glitches

--- ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/impl/UBorder.java
+++ ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/impl/UBorder.java
@@ -18,6 +18,7 @@ public class UBorder implements Border {
 
     private static final Color BORDER = new Color(0x95989e);
     private static final Color SHADOW = new Color(0x28000000, true);
+    private static final Insets BASEINSETS = new Insets(1, 3, 1, 3);
 
     private final boolean borderVisible;
     private final int clipLeft;
@@ -28,14 +29,26 @@ public class UBorder implements Border {
         this(borderVisible, false, false);
     }
 
+    public UBorder(boolean borderVisible, Insets baseInsets) {
+        this(borderVisible, baseInsets, false, false);
+    }
+
     public UBorder(boolean borderVisible, boolean clipLeft, boolean clipRight) {
+        this(borderVisible, null, clipLeft, clipRight);
+    }
+
+    public UBorder(boolean borderVisible, Insets baseInsets, boolean clipLeft, boolean clipRight) {
         super();
 
         this.borderVisible = borderVisible;
         this.clipLeft = clipLeft ? 3 : 0;
         this.clipRight = clipRight ? 3 : 0;
 
-        insets = new Insets(4, 5 - this.clipLeft, 4, 5 - this.clipRight);
+        if (baseInsets == null) {
+            baseInsets = BASEINSETS;
+        }
+
+        insets = new Insets(3 + baseInsets.top, (3 + baseInsets.left) - this.clipLeft, 3 + baseInsets.bottom, (3 + baseInsets.right) - this.clipRight);
     }
 
     public boolean isBorderVisible() {
@@ -119,8 +132,26 @@ public class UBorder implements Border {
         g2d.setStroke(stroke);
 
         // draw the inner stuff
-        g2d.setColor(c.getBackground());
-        g2d.drawRect(x + 3, y + 3, width - 7, height - 7);
+
+        if (c.isOpaque()) {
+            g2d.setColor(c.getBackground());
+
+            if (insets.top > 3) {
+                g2d.drawRect(x + 3, y + 3, width - 7, insets.top - 4);
+            }
+
+            if (insets.bottom > 3) {
+                g2d.drawRect(x + 3, (y + height) - insets.bottom, width - 7, insets.bottom - 4);
+            }
+
+            if (insets.left > 3) {
+                g2d.fillRect(x + 3, y + insets.top, insets.left - 3, height - insets.top - insets.bottom);
+            }
+
+            if (insets.right > 3) {
+                g2d.fillRect((x + width) - insets.right, y + insets.top, insets.right - 3, height - insets.top - insets.bottom);
+            }
+        }
 
         // draw the original border
         if (c.isEnabled()) {
