cbcaf329184ae968d88e525da30451115f623b51
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com>
2012-12-12 09:06:31 +0000

    added one pixel to the height of tree elements to better separate the icons

--- ulrice-core/src/main/java/net/ulrice/frame/impl/navigation/ModuleTreeCellRenderer.java
+++ ulrice-core/src/main/java/net/ulrice/frame/impl/navigation/ModuleTreeCellRenderer.java
@@ -9,33 +9,38 @@ import net.ulrice.module.IFModuleTitleProvider.Usage;
 
 public class ModuleTreeCellRenderer extends DefaultTreeCellRenderer {
 
-	@Override
-	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded,
-			boolean leaf, int row, boolean hasFocus) {
+    private static final long serialVersionUID = 8052377035977724216L;
 
-		Object renderValue = value;
-		
-		if(value instanceof ModuleTreeNode) {
-		    ModuleTreeNode node = (ModuleTreeNode) renderValue;
-			StringBuffer buffer = new StringBuffer();
-		    switch(node.getNodeType()) {
-		    	case ModuleGroup:
-	                buffer.append(node.getModuleGroup().getTitle());
-		    		break;
-		    	case Module:
-	    			buffer.append(node.getModule().getModuleTitle(Usage.ModuleTree));
-		    		break;
-		    	case ProfiledModule:
-	    			buffer.append(node.getProfiledModule().getProfileId());
-		    		break;
-	    		default:
-	    			break;
-		    }
-			renderValue = buffer.toString();
-		}
-	
-		
-		return super.getTreeCellRendererComponent(tree, renderValue, sel, expanded, leaf, row, hasFocus);
-	}
+    public ModuleTreeCellRenderer() {
+        super();
+    }
+
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded,
+        boolean leaf, int row, boolean hasFocus) {
+
+        Object renderValue = value;
+
+        if (value instanceof ModuleTreeNode) {
+            ModuleTreeNode node = (ModuleTreeNode) renderValue;
+            StringBuffer buffer = new StringBuffer();
+            switch (node.getNodeType()) {
+                case ModuleGroup:
+                    buffer.append(node.getModuleGroup().getTitle());
+                    break;
+                case Module:
+                    buffer.append(node.getModule().getModuleTitle(Usage.ModuleTree));
+                    break;
+                case ProfiledModule:
+                    buffer.append(node.getProfiledModule().getProfileId());
+                    break;
+                default:
+                    break;
+            }
+            renderValue = buffer.toString();
+        }
+
+        return super.getTreeCellRendererComponent(tree, renderValue, sel, expanded, leaf, row, hasFocus);
+    }
 
 }
