232fd1db34625e5afd52b057bacad49a55e13a64
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2013-01-11 12:16:52 +0000

    Record screens

--- trunk/ulrice-recorder/src/main/java/net/ulrice/recorder/Recorder.java
+++ trunk/ulrice-recorder/src/main/java/net/ulrice/recorder/Recorder.java
@@ -9,74 +9,148 @@ import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.AWTEventListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.awt.geom.AffineTransform;
-import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+import java.awt.image.VolatileImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.SwingUtilities;
 
 import net.ulrice.Ulrice;
 
 public class Recorder implements AWTEventListener {
-	
-	private String title = "Recording";
-	private String description;
+
 	private Component component;
 	private LinkedList<RecordedScreen> screens;
 	private File outputDirectory;
+	private RecorderDialog dialog = new RecorderDialog();
+	private String title;
 
 	public Recorder(Component component, File outputDirectory) {
 		this.component = component;
 		this.outputDirectory = outputDirectory;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public void setDescription(String description) {
-		this.description = description;
+
+		dialog.getStopButton().addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				stop();
+			}
+
+		});
+
+		dialog.getRecordButton().addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				start();
+			}
+
+		});
+
+		dialog.getSaveButton().addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					stop();
+					save();
+				} catch (IOException e1) {
+					Ulrice.getMessageHandler().handleException(e1);
+				}
+			}
+
+		});
+
+		dialog.pack();
+		dialog.setVisible(true);
+
 	}
 
 	public void start() {
 		screens = new LinkedList<RecordedScreen>();
+
+		dialog.getRecordButton().setSelected(true);
+
+		dialog.getRecordButton().setSelected(false);
+		dialog.getScreenTitle().setEnabled(false);
+		dialog.getScreenDescription().setEnabled(false);
+		
+		dialog.getTitleField().setText("");
+		dialog.getCategoryField().setText("");
+		dialog.getDescriptionArea().setText("");
+		dialog.getScreenshot().setIcon(null);
+		dialog.getScreenTitle().setText("");
+		dialog.getScreenDescription().setText("");
+		
 		Toolkit.getDefaultToolkit().removeAWTEventListener(this);
 		Toolkit.getDefaultToolkit().addAWTEventListener(this, AWTEvent.KEY_EVENT_MASK);
 	}
 
 	public void stop() {
 		Toolkit.getDefaultToolkit().removeAWTEventListener(this);
+		updateScreenTexts();
+		
+		title = dialog.getTitleField().getText().replace(" ", "_");
+		
+		
 	}
 	
+	public String getTitle() {
+		return title;
+	}
+
 	public void setTitleToLastScreen(String title) {
-		if(screens.size() > 0) {
+		if (screens.size() > 0) {
 			screens.get(screens.size() - 1).setTitle(title);
 		}
 	}
-	
+
 	public void setDescriptionToLastScreen(String description) {
-		if(screens.size() > 0) {
+		if (screens.size() > 0) {
 			screens.get(screens.size() - 1).setTitle(description);
 		}
 	}
 
 	public void save() throws IOException {
-		PrintWriter pw = new PrintWriter(new File(outputDirectory, title + ".xml"));
+		if(screens.isEmpty()) {
+			return;
+		}
+		
+		List<File> files = new ArrayList<File>();
+		File xmlFile = new File(outputDirectory, getTitle() + ".xml");
+		files.add(xmlFile);
+		
+		PrintWriter pw = new PrintWriter(xmlFile);				
 		pw.println("<RecordedScreens>");
-		pw.println(String.format("  <Title>%s</Title>", title));
-		pw.println(String.format("  <Description>%s</Description>", description));
+		pw.println(String.format("  <Title>%s</Title>", dialog.getTitleField().getText()));
+		pw.println(String.format("  <Category>%s</Category>", dialog.getCategoryField().getText()));
+		pw.println(String.format("  <Description>%s</Description>", dialog.getDescriptionArea().getText()));
 		pw.println("  <Screens>");
 		try {
-			for(int i = 0; i < screens.size(); i++) {
+			for (int i = 0; i < screens.size(); i++) {
 				RecordedScreen screen = screens.get(i);
-				File screenshot = new File(outputDirectory, "screen-" + i + ".jpg");
+				File screenshot = new File(outputDirectory, getTitle() + "-screen-" + i + ".jpg");
+				files.add(screenshot);
+				File screenshot_small = new File(outputDirectory, getTitle() + "-screen-" + i + "_small.jpg");
+				files.add(screenshot_small);
+				
 				ImageIO.write(screen.getFullImage(), "JPG", screenshot);
+				ImageIO.write(screen.getSmallImage(), "JPG", screenshot_small);
 				pw.println("    <Screen>");
 				pw.println(String.format("      <ScreenTitle>%s</ScreenTitle>", screen.getTitle()));
 				pw.println(String.format("      <ScreenDescription>%s</ScreenDescription>", screen.getDescription()));
@@ -87,16 +161,60 @@ public class Recorder implements AWTEventListener {
 			pw.println("</RecordedScreens>");
 		} finally {
 			pw.close();
-		}		
+		}
+		
+		compressOutput(files);
 	}
 	
+	public void compressOutput(List<File> files) {
+	    
+	    byte[] buf = new byte[1024];
+	    
+	    try {
+	        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(getTitle() + ".xml.zip"));
+	    
+	        for (int i=0; i<files.size(); i++) {
+	            FileInputStream in = new FileInputStream(files.get(i));
+	            out.putNextEntry(new ZipEntry(files.get(i).getName()));
+	            int len;
+	            while ((len = in.read(buf)) > 0) {
+	                out.write(buf, 0, len);
+	            }
+	            out.closeEntry();
+	            in.close();
+	        }
+	    
+	        out.close();
+	        
+	        for(File file : files) {
+	        	file.delete();
+	        }
+	    } catch (IOException e) {
+	    	Ulrice.getMessageHandler().handleException(e);
+	    }
+	}
+
 	@Override
 	public void eventDispatched(AWTEvent e) {
 		KeyEvent keyEvent = (KeyEvent) e;
-		
+
 		if (keyEvent.getID() == KeyEvent.KEY_RELEASED && KeyEvent.VK_F12 == keyEvent.getKeyCode()) {
 			try {
-				screens.add(recordScreen());
+				
+				
+				updateScreenTexts();
+
+				RecordedScreen screen = recordScreen();
+				screens.add(screen);
+				dialog.getScreenTitle().setEnabled(true);
+				dialog.getScreenDescription().setEnabled(true);
+
+				dialog.getScreenshot().setIcon(new ImageIcon(screen.getSmallImage()));
+				dialog.getScreenTitle().setText("");
+				dialog.getScreenDescription().setText("");
+				dialog.getContentPane().invalidate();
+				dialog.getContentPane().repaint();
+
 			} catch (AWTException e1) {
 				Ulrice.getMessageHandler().handleException(e1);
 			} catch (IOException e1) {
@@ -105,39 +223,85 @@ public class Recorder implements AWTEventListener {
 		}
 	}
 
+	private void updateScreenTexts() {
+		if (screens.size() > 0) {
+			RecordedScreen lastScreen = screens.get(screens.size() - 1);
+			lastScreen.setTitle(dialog.getScreenTitle().getText());
+			lastScreen.setDescription(dialog.getScreenDescription().getText());
+		}
+	}
+
 	public RecordedScreen recordScreen() throws AWTException, IOException {
-		System.out.println("Record!");
 		BufferedImage screenCapture = getScreenShotFromComponent(component);
 
 		Image cursor = ImageIO.read(Recorder.class.getResourceAsStream("pointer.png"));
-		Point mousePointer = MouseInfo.getPointerInfo().getLocation(); 
+		Point mousePointer = MouseInfo.getPointerInfo().getLocation();
 		SwingUtilities.convertPointFromScreen(mousePointer, component);
-		
+
+		Graphics2D graphics2D = screenCapture.createGraphics();
+		graphics2D.drawImage(cursor, mousePointer.x, mousePointer.y, 25, 39, null);
+
 		int width = screenCapture.getWidth();
 		int height = screenCapture.getHeight();
 		int scaledWidth = 320;
 		int scaledHeight = 200;
 
-		double scaleW = 100.0d / width * scaledWidth;
-		double scaleH = 100.0d / height * scaledHeight;
+		double scaleW = 1.0d / width * scaledWidth;
+		double scaleH = 1.0d / height * scaledHeight;
+		double scale = Math.min(scaleW, scaleH);
 
-		Graphics2D graphics2D = screenCapture.createGraphics();
-		graphics2D.drawImage(cursor, mousePointer.x, mousePointer.y, 25, 39, null);
-
-		BufferedImage scaledImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		AffineTransform at = new AffineTransform();
-		at.scale(Math.min(scaleW, scaleH), Math.min(scaleW, scaleH));
-		AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
-		scaledImage = scaleOp.filter(screenCapture, scaledImage);
+		BufferedImage image = screenCapture;
+		Image smallerImg = image.getScaledInstance(Double.valueOf(width * scale).intValue(), Double.valueOf(height * scale).intValue(), Image.SCALE_SMOOTH);
 
 		RecordedScreen screen = new RecordedScreen();
 		screen.setFullImage(screenCapture);
-		screen.setSmallImage(scaledImage);
+		screen.setSmallImage(toBufferedImage(smallerImg, BufferedImage.TYPE_BYTE_INDEXED));
 
 		return screen;
 	}
 
-	private BufferedImage getScreenShotFromComponent(Component component) {		
+	public BufferedImage toBufferedImage(final Image image, final int type) {
+		if (image instanceof BufferedImage)
+			return (BufferedImage) image;
+		if (image instanceof VolatileImage)
+			return ((VolatileImage) image).getSnapshot();
+		loadImage(image);
+		final BufferedImage buffImg = new BufferedImage(image.getWidth(null), image.getHeight(null), type);
+		final Graphics2D g2 = buffImg.createGraphics();
+		g2.drawImage(image, null, null);
+		g2.dispose();
+		return buffImg;
+	}
+
+	private void loadImage(final Image image) {
+		class StatusObserver implements ImageObserver {
+			boolean imageLoaded = false;
+
+			public boolean imageUpdate(final Image img, final int infoflags, final int x, final int y, final int width, final int height) {
+				if (infoflags == ALLBITS) {
+					synchronized (this) {
+						imageLoaded = true;
+						notify();
+					}
+					return true;
+				}
+				return false;
+			}
+		}
+		final StatusObserver imageStatus = new StatusObserver();
+		synchronized (imageStatus) {
+			if (image.getWidth(imageStatus) == -1 || image.getHeight(imageStatus) == -1) {
+				while (!imageStatus.imageLoaded) {
+					try {
+						imageStatus.wait();
+					} catch (InterruptedException ex) {
+					}
+				}
+			}
+		}
+	}
+
+	private BufferedImage getScreenShotFromComponent(Component component) {
 		BufferedImage image = new BufferedImage(component.getWidth(), component.getHeight(), BufferedImage.TYPE_INT_RGB);
 		component.paint(image.getGraphics());
 		return image;
--- trunk/ulrice-recorder/src/main/java/net/ulrice/recorder/RecorderDialog.java
+++ trunk/ulrice-recorder/src/main/java/net/ulrice/recorder/RecorderDialog.java
@@ -1,10 +1,26 @@
 package net.ulrice.recorder;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.net.URL;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JToggleButton;
+import javax.swing.border.EtchedBorder;
+
+import net.ulrice.Ulrice;
 
 /**
  * Dialog for controlling the recorder
@@ -12,16 +28,137 @@ import javax.swing.JTextField;
  * @author christof
  */
 public class RecorderDialog extends JDialog {
-	
-	private JTextField title;
-	private JTextArea description;
-	
+
+	private static final long serialVersionUID = 1L;
+
+	private JTextField titleField;
+	private JTextField categoryField;
+	private JTextArea descriptionArea;
+
 	private JLabel screenshot;
 	private JTextField screenTitle;
 	private JTextArea screenDescription;
-	
-	private JButton recordButton;
+
+	private JToggleButton recordButton;
 	private JButton stopButton;
 	private JButton saveButton;
 
+	public RecorderDialog() {
+		super(Ulrice.getMainFrame().getFrame());
+
+		setTitle("Recorder");
+
+		titleField = new JTextField();
+		categoryField = new JTextField();
+		descriptionArea = new JTextArea();
+		descriptionArea.setRows(5);
+
+		JPanel titlePanel = new JPanel();
+		titlePanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(2, 2, 4, 2),
+				BorderFactory.createCompoundBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED), BorderFactory.createEmptyBorder(2, 2, 2, 2))));
+		titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
+		titlePanel.add(createComponentWithLabel("Title", titleField, LEFT_ALIGNMENT));
+		titlePanel.add(createComponentWithLabel("Category", categoryField, LEFT_ALIGNMENT));
+		titlePanel.add(createComponentWithLabel("Description", new JScrollPane(descriptionArea), LEFT_ALIGNMENT));
+
+		screenshot = new JLabel();
+		screenshot.setOpaque(true);
+		screenshot.setHorizontalAlignment(JLabel.CENTER);
+		screenshot.setVerticalTextPosition(JLabel.BOTTOM);
+		screenshot.setHorizontalTextPosition(JLabel.CENTER);
+		screenshot.setBackground(Color.BLACK);
+		screenshot.setAlignmentX(CENTER_ALIGNMENT);
+		screenshot.setAlignmentY(CENTER_ALIGNMENT);
+		screenshot.setMinimumSize(new Dimension(320, 200));
+		screenshot.setPreferredSize(new Dimension(320, 200));
+
+		screenTitle = new JTextField();
+		screenDescription = new JTextArea();
+		screenDescription.setRows(5);
+
+		JPanel screenPanel = new JPanel(new BorderLayout());
+		screenPanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+		screenPanel.add(createComponentWithLabel("Screen Title", screenTitle, LEFT_ALIGNMENT), BorderLayout.NORTH);
+		screenPanel.add(screenshot, BorderLayout.CENTER);
+		screenPanel.add(createComponentWithLabel("Screen Description", new JScrollPane(screenDescription), LEFT_ALIGNMENT), BorderLayout.SOUTH);
+
+		stopButton = new JButton(createIcon("stop.png"));
+		recordButton = new JToggleButton(createIcon("record.png"));
+		saveButton = new JButton(createIcon("save.png"));
+
+		screenTitle.setEnabled(false);
+		screenDescription.setEnabled(false);
+
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+		buttonPanel.add(recordButton);
+		buttonPanel.add(stopButton);
+		buttonPanel.add(saveButton);
+
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(titlePanel, BorderLayout.NORTH);
+		getContentPane().add(screenPanel, BorderLayout.CENTER);
+		getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+	}
+
+	private Icon createIcon(String iconName) {
+		URL iconUrl = getClass().getResource(iconName);
+		if (iconUrl != null) {
+			return new ImageIcon(iconUrl);
+		}
+
+		return null;
+	}
+
+	public JPanel createComponentWithLabel(String labelText, JComponent component, float componentAlignment) {
+		JLabel label = new JLabel(labelText);
+		label.setAlignmentX(LEFT_ALIGNMENT);
+		label.setLabelFor(component);
+
+		component.setAlignmentX(componentAlignment);
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.add(label);
+		panel.add(component);
+
+		return panel;
+	}
+
+	public JLabel getScreenshot() {
+		return screenshot;
+	}
+
+	public JTextField getTitleField() {
+		return titleField;
+	}
+
+	public JTextField getCategoryField() {
+		return categoryField;
+	}
+
+	public JTextArea getDescriptionArea() {
+		return descriptionArea;
+	}
+
+	public JTextField getScreenTitle() {
+		return screenTitle;
+	}
+
+	public JTextArea getScreenDescription() {
+		return screenDescription;
+	}
+
+	public JToggleButton getRecordButton() {
+		return recordButton;
+	}
+
+	public JButton getStopButton() {
+		return stopButton;
+	}
+
+	public JButton getSaveButton() {
+		return saveButton;
+	}
 }
