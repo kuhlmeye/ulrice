a35a52b4c343d5b4a9a55594ca8189a8a17b08f1
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2013-02-04 15:50:43 +0000

    added controller to generic parameters, fixed some process violations

--- trunk/ulrice-core/src/main/java/net/ulrice/module/ControllerProviderCallback.java
+++ trunk/ulrice-core/src/main/java/net/ulrice/module/ControllerProviderCallback.java
@@ -6,13 +6,13 @@ import net.ulrice.module.exception.ModuleInstantiationException;
 /**
  * Callback for the instantiation of a controller.
  */
-public abstract class ControllerProviderCallback<T extends IFController> {
+public abstract class ControllerProviderCallback<CONTROLLER_TYPE extends IFController> {
 
 	/**
 	 * Called by the module manager after controller initialization is finished
 	 * and the controller is completely registered at the application.
 	 */
-	public void onControllerReady(T controller) {		
+	public void onControllerReady(CONTROLLER_TYPE controller) {		
 	}
 
 	/**
@@ -20,7 +20,7 @@ public abstract class ControllerProviderCallback<T extends IFController> {
 	 * controller. The controller is not yet fully registered at the
 	 * application. In this method basic data initialization could be done.
 	 */
-	public void onControllerInitialization(T controller) {	
+	public void onControllerInitialization(CONTROLLER_TYPE controller) {	
 	}
 
 	/**
