5c017d43b5812b6e5cdbdf6886e0d51a2298bb34
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2013-03-03 20:01:59 +0000

    added horizontal and vertical scrollpane

---
+++ trunk/ulrice-core/src/main/java/net/ulrice/ui/components/HorizontalScrollPane.java
@@ -0,0 +1,44 @@
+package net.ulrice.ui.components;
+
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.JScrollPane;
+
+/**
+ * A {@link JScrollPane} that only scrolls horizontally. In contrast to a default {@link JScrollPane}, it sets the
+ * preferred size of the component to fit its height, instead of just hiding the vertical scrollbar. When showing the
+ * horizontal scrollbar, it tries to enlarge its own size to keep the size of the viewport (this works, for example, in
+ * the north or south part of a BorderLayout).
+ * 
+ * @author Manfred Hantschel
+ */
+public class HorizontalScrollPane extends AbstractLimitedScrollPane {
+
+    private static final long serialVersionUID = 4786312248219326535L;
+
+    public HorizontalScrollPane() {
+        this(HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    }
+
+    public HorizontalScrollPane(Component view, int hsbPolicy) {
+        super(view, VERTICAL_SCROLLBAR_NEVER, hsbPolicy);
+    }
+
+    public HorizontalScrollPane(Component view) {
+        this(view, HORIZONTAL_SCROLLBAR_AS_NEEDED);
+    }
+
+    public HorizontalScrollPane(int hsbPolicy) {
+        super(VERTICAL_SCROLLBAR_NEVER, hsbPolicy);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected boolean isUpdatePreferredSizeNeeded(Dimension currentPreferredSize, Dimension expectedPreferredSize) {
+        return currentPreferredSize.height != expectedPreferredSize.height;
+    }
+
+}
