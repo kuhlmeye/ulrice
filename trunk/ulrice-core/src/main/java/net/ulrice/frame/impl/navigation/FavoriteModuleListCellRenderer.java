a3bd4bbefedec99f79504645e77fa895957588ef
ckuhlmeyer@gmail.com <ckuhlmeyer@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2013-05-12 12:15:49 +0000

    Added icon to renderer

--- trunk/ulrice-core/src/main/java/net/ulrice/frame/impl/navigation/FavoriteModuleListCellRenderer.java
+++ trunk/ulrice-core/src/main/java/net/ulrice/frame/impl/navigation/FavoriteModuleListCellRenderer.java
@@ -3,12 +3,15 @@ package net.ulrice.frame.impl.navigation;
 import java.awt.Component;
 
 import javax.swing.DefaultListCellRenderer;
+import javax.swing.Icon;
+import javax.swing.JLabel;
 import javax.swing.JList;
 
 import net.ulrice.Ulrice;
 import net.ulrice.module.IFController;
 import net.ulrice.module.IFModule;
 import net.ulrice.module.IFModuleTitleProvider.Usage;
+import net.ulrice.module.ModuleIconSize;
 
 /**
  * Renders a node in the favorite module list
@@ -23,9 +26,11 @@ public class FavoriteModuleListCellRenderer extends DefaultListCellRenderer {
 	public Component getListCellRendererComponent(JList list, Object value, int idx, boolean sel, boolean hasFocus) {
 		Object renderValue = value;
 		
+		Icon icon = null;
 		if(value instanceof IFModule) {						
 			@SuppressWarnings("rawtypes")
 			IFModule<?> module = (IFModule) renderValue;
+			icon = module.getIcon(ModuleIconSize.Size_16x16);
 			renderValue = module.getModuleTitle(Usage.ModuleTree);
 		}
 
@@ -33,7 +38,10 @@ public class FavoriteModuleListCellRenderer extends DefaultListCellRenderer {
 			IFController controller = (IFController) value;			
 			renderValue = Ulrice.getModuleManager().getTitleProvider(controller).getModuleTitle(Usage.ModuleTree);
 		}
-		
-		return super.getListCellRendererComponent(list, renderValue, idx, sel, hasFocus);
+		JLabel label = (JLabel) super.getListCellRendererComponent(list, renderValue, idx, sel, hasFocus);
+		if(icon != null) {
+			label.setIcon(icon);
+		}
+		return label;
 	}
 }
