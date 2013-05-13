8ed731b260d090adacada9c557a33fbc63b39e61
Gero.Kuss@gmail.com <Gero.Kuss@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2013-05-13 08:36:44 +0000

    Add 'addTreeWillExpandListener'

--- ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTreeTableComponent.java
+++ ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTreeTableComponent.java
@@ -11,6 +11,7 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
+import javax.swing.event.TreeWillExpandListener;
 import javax.swing.tree.TreePath;
 
 import net.ulrice.Ulrice;
@@ -358,4 +359,9 @@ public class UTreeTableComponent extends UTableComponent implements ExpandColaps
             }
         }
     }
+    
+    public void addTreeWillExpandListener(TreeWillExpandListener tel) {
+        tree.addTreeWillExpandListener(tel);
+    }
+    
 }
