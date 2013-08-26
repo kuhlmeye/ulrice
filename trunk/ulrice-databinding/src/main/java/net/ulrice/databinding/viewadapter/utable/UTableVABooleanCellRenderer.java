cb29d5d8dfcc83739ac47f96e88faba27a5e9547
dahu4wa@gmail.com <dahu4wa@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2013-08-26 13:02:56 +0000

    if the value is readonly and null, we display a empty cell (because it is neither false nor true and null values do not appear in the UTableVAFilter when searching for false)

--- trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTableVABooleanCellRenderer.java
+++ trunk/ulrice-databinding/src/main/java/net/ulrice/databinding/viewadapter/utable/UTableVABooleanCellRenderer.java
@@ -2,42 +2,49 @@ package net.ulrice.databinding.viewadapter.utable;
 
 import java.awt.Component;
 
+import javax.swing.BorderFactory;
 import javax.swing.JCheckBox;
+import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 
 /**
- * 
  * UTableVABooleanCellRenderer to boolean and Boolean within the UTable as a JCheckBox.
  * 
  * @author apunahassaphemapetilon
- *
  */
 public class UTableVABooleanCellRenderer extends UTableVADefaultRenderer {
+    private static final long serialVersionUID = -2732626862848774169L;
 
-	private JCheckBox renderer;
+    private JCheckBox renderer;
+    private JPanel emptyRenderer;
 
-	public UTableVABooleanCellRenderer() {
-		super();
-		this.renderer = new JCheckBox();
-		this.renderer.setOpaque(true);
-		
-	}
+    public UTableVABooleanCellRenderer() {
+        super();
+        this.renderer = new JCheckBox();
+        this.renderer.setOpaque(true);
+        this.emptyRenderer = new JPanel();
+    }
 
-	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value,
-			boolean isSelected, boolean hasFocus, int row, int column) {
-		renderer.setHorizontalAlignment(SwingConstants.CENTER);
-		if (value != null
-				&& (Boolean.class.equals(value.getClass()) || Boolean.TYPE
-						.equals(value.getClass()))) {
-			renderer.setSelected(((Boolean) value).booleanValue());
-			renderer.setEnabled(table.isCellEditable(row, column));
-		} else {
-			renderer.setSelected(false);
-			renderer.setEnabled(table.isCellEditable(row, column));
-		}
-		return adaptComponent(table, isSelected, row, column, renderer);
-	}
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        renderer.setHorizontalAlignment(SwingConstants.CENTER);
+        if (value != null && (Boolean.class.equals(value.getClass()) || Boolean.TYPE.equals(value.getClass()))) {
+            renderer.setSelected(((Boolean) value).booleanValue());
+            renderer.setEnabled(table.isCellEditable(row, column));
+            return adaptComponent(table, isSelected, row, column, renderer);
+        }
+        else {
+            if (table.isCellEditable(row, column)) {
+                renderer.setSelected(false);
+                renderer.setEnabled(table.isCellEditable(row, column));
+                return adaptComponent(table, isSelected, row, column, renderer);
+            }
+            else {
+                emptyRenderer.setBorder(BorderFactory.createEmptyBorder());
+                return adaptComponent(table, isSelected, row, column, emptyRenderer);
+            }
+        }
+    }
 
 }
