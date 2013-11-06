9a6aac8bc1132d4d594a1db2cbc907eb0f15c729
dkeller1979@gmail.com <dkeller1979@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2013-11-06 12:52:57 +0000

    revert to revision 1202

--- trunk/ulrice-core/src/main/java/net/ulrice/message/TranslationConstants.java
+++ trunk/ulrice-core/src/main/java/net/ulrice/message/TranslationConstants.java
@@ -5,5 +5,4 @@ public interface TranslationConstants {
 	static final String ALL = "All";
 	static final String YES = "Yes";
 	static final String NO = "No";
-	static final String UNIQUE_KEY_CONSTRAINT_ERROR = "UniqueKeyConstraintError";
 }
