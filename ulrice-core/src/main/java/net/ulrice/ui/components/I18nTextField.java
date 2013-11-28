359697a95cd3e4dff331801689186f597b2ce258
andre.jacobs74@gmail.com <andre.jacobs74@gmail.com@7713110d-924a-4a19-f96a-9e145019c34c>
2013-11-28 09:54:43 +0000

    added limit support for i18n textfield and textarea

--- ulrice-core/src/main/java/net/ulrice/ui/components/I18nTextField.java
+++ ulrice-core/src/main/java/net/ulrice/ui/components/I18nTextField.java
@@ -11,39 +11,45 @@ import javax.swing.UIManager;
 public class I18nTextField extends I18nTextComponent {
 
 	private static final long serialVersionUID = 1L;
-
-	public I18nTextField() {
-		super(new JTextField() {
-			private static final long serialVersionUID = 5152597750522473770L;
-
-			public void updateUI() {
-				super.updateUI();
-				setOpaque(false);
-
-				Color sfBackground = UIManager.getColor("I18nTextField.background");
-				if (sfBackground != null) {
-					setBackground(sfBackground);
-				}
-
-				Color sfForeground = UIManager.getColor("I18nTextField.foreground");
-				if (sfForeground != null) {
-					setForeground(sfForeground);
-				}
-
-				setBorder(BorderFactory.createEmptyBorder());
-
-				int sfWidth = UIManager.getInt("I18nTextField.fieldWidth");
-				if (sfWidth > 0) {
-					setPreferredSize(new Dimension(sfWidth, getHeight()));
-				}
-			}
-		});
 	
-		getTextComponent().setBorder(BorderFactory.createEmptyBorder());
-		
-		setBackground(getTextComponent().getBackground());
+	public I18nTextField() {
+	    super(new JTextField() {
+            private static final long serialVersionUID = 6274378024308897184L;
 
-		add(getLocaleSelector(), BorderLayout.WEST);
-		add(getTextComponent(), BorderLayout.CENTER);
+            public void updateUI() {
+	            super.updateUI();
+	            setOpaque(false);
+
+	            Color sfBackground = UIManager.getColor("I18nTextField.background");
+	            if (sfBackground != null) {
+	                setBackground(sfBackground);
+	            }
+
+	            Color sfForeground = UIManager.getColor("I18nTextField.foreground");
+	            if (sfForeground != null) {
+	                setForeground(sfForeground);
+	            }
+
+	            setBorder(BorderFactory.createEmptyBorder());
+
+	            int sfWidth = UIManager.getInt("I18nTextField.fieldWidth");
+	            if (sfWidth > 0) {
+	                setPreferredSize(new Dimension(sfWidth, getHeight()));
+	            }
+	        }
+	    });
+	    
+        getTextComponent().setBorder(BorderFactory.createEmptyBorder());
+        
+        setBackground(getTextComponent().getBackground());
+
+        add(getLocaleSelector(), BorderLayout.WEST);
+        add(getTextComponent(), BorderLayout.CENTER);
+	}
+	
+	public void setMaxLength(int maxLength) {
+	    if (getTextComponent().getDocument().getClass() == I18nTextComponent.I18nPlainDocument.class) {
+	       ((I18nTextComponent.I18nPlainDocument) getTextComponent().getDocument()).setMaxLength(maxLength); 
+	    }
 	}
 }
