c162ff2a4959fb239664d1c5b1d67f94d1549982
manfred.hantschel@gmail.com <manfred.hantschel@gmail.com>
2015-03-13 12:08:51 +0000

    fixed test failure by more robust code (merged)

--- ulrice-databinding/src/main/java/net/ulrice/databinding/validation/impl/NotNullValidator.java
+++ ulrice-databinding/src/main/java/net/ulrice/databinding/validation/impl/NotNullValidator.java
@@ -5,36 +5,44 @@ import net.ulrice.databinding.IFBinding;
 import net.ulrice.databinding.validation.AbstractValidator;
 import net.ulrice.databinding.validation.ValidationError;
 import net.ulrice.databinding.validation.ValidationResult;
+import net.ulrice.message.TranslationProvider;
 import net.ulrice.message.TranslationUsage;
 
 /**
- * 
- * @author apunahassaphemapetilon@hotmail.com
- *
- * Rawtypes is ok as we just check if the object is null.
+ * @author apunahassaphemapetilon@hotmail.com Rawtypes is ok as we just check if the object is null.
  */
 
 @SuppressWarnings("rawtypes")
 public class NotNullValidator extends AbstractValidator {
 
-	@Override
-	protected ValidationResult validate(IFBinding bindingId, Object attribute, Object rawAttribute) {
-		ValidationResult result = new ValidationResult();
-				
+    @Override
+    protected ValidationResult validate(IFBinding bindingId, Object attribute, Object rawAttribute) {
+        ValidationResult result = new ValidationResult();
+
         if (attribute == null) {
+            TranslationProvider translationProvider = Ulrice.getTranslationProvider();
 
             if (bindingId.getOriginalValue() instanceof Number) {
-                result.addValidationError(new ValidationError(bindingId, Ulrice.getTranslationProvider()
-                    .getUlriceTranslation(TranslationUsage.ValidationLabel, "NumberValueExpected").getText(), null));
+                if (translationProvider != null) {
+                    result.addValidationError(new ValidationError(bindingId, translationProvider.getUlriceTranslation(TranslationUsage.ValidationLabel, "NumberValueExpected")
+                        .getText(), null));
+                }
+                else {
+                    result.addValidationError(new ValidationError(bindingId, "attribute is not a number", null));
+                }
             }
             else {
-                // TODO Tobias Internationalize
-                result.addValidationError(new ValidationError(bindingId, Ulrice.getTranslationProvider()
-                    .getUlriceTranslation(TranslationUsage.ValidationLabel, "NotNullValueExpected").getText(), null));
+                if (translationProvider != null) {
+                    result.addValidationError(new ValidationError(bindingId, translationProvider.getUlriceTranslation(TranslationUsage.ValidationLabel, "NotNullValueExpected")
+                        .getText(), null));
+                }
+                else {
+                    result.addValidationError(new ValidationError(bindingId, "attribute must not be null!", null));
+                }
             }
-
         }
+
         return result;
-	}
+    }
 
 }
